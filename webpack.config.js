d664663ce2ebc6d46f860b0fb6d2098b6f06de13
David Vargas <dvargas92495@gmail.com>
2020-08-25 17:43:38 -0400

    remove node target

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const extensions = fs.readdirSync("./src/entries/");
 const entry = Object.fromEntries(extensions.map(e => [e.substring(0, e.length - 3), `./src/entries/${e}`]));
 
 module.exports = {
-  target: "node",
   entry,
   resolve: {
     extensions: [".ts", ".js"],
