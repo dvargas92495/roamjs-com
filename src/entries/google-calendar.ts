4b282c88138c54127e49b12f74b9cb0263c1890d
David Vargas <dvargas92495@gmail.com>
2020-08-25 17:26:01 -0400

    Fetching Events - card-44210689

--- src/entries/google-calendar.ts
+++ src/entries/google-calendar.ts
@@ -1,1 +1,19 @@
-console.log('Google Calendar Integration coming soon!')
+const { google } = require("googleapis");
+
+const importCalendarListener = (e: MouseEvent) => {
+  const target = e.target as HTMLElement;
+  if (
+    target &&
+    target.tagName === "BUTTON" &&
+    target.innerText === "Import Google Calendar"
+  ) {
+    const apiKey = document.getElementById("google-calendar-api-key").nodeValue;
+
+    const calendar = google.calendar({
+      version: "v3",
+      auth: apiKey,
+    });
+    console.log("trigger import events");
+    calendar.list().then((cals: any) => console.log(cals));
+  }
+};
