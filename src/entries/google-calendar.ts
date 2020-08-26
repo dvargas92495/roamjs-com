ce5daa7aaa11fae9eab72de6c42fbfa423e3f68c
David Vargas <dvargas92495@gmail.com>
2020-08-26 15:41:24 -0400

    Fix params

--- src/entries/google-calendar.ts
+++ src/entries/google-calendar.ts
@@ -5,23 +5,33 @@ const importCalendarListener = (e: MouseEvent) => {
     target.tagName === "BUTTON" &&
     target.innerText.toUpperCase() === "IMPORT GOOGLE CALENDAR"
   ) {
-    const blocks = Array.from(document.getElementsByClassName('roam-block'));
-    const calendarBlock = blocks.find(b => {
-        const blockSpan = b.children[0];
-        if (!blockSpan) { return false;}
-        return blockSpan.childNodes.length === 2 && 
-        blockSpan.children[0].tagName === "STRONG" && blockSpan.children[0].innerHTML.toUpperCase() === 'GOOGLE CALENDAR:';
+    const blocks = Array.from(document.getElementsByClassName("roam-block"));
+    const calendarBlock = blocks.find((b) => {
+      const blockSpan = b.children[0];
+      if (!blockSpan) {
+        return false;
+      }
+      return (
+        blockSpan.childNodes.length === 2 &&
+        blockSpan.children[0].tagName === "STRONG" &&
+        blockSpan.children[0].innerHTML.toUpperCase() === "GOOGLE CALENDAR:"
+      );
     });
     const calendarId = calendarBlock.children[0].childNodes[1].nodeValue;
     if (!calendarId) {
       console.warn("Could not find calendar ID!");
       return;
     }
-    const apiBlock = blocks.find(b => {
-        const blockSpan = b.children[0];
-        if (!blockSpan) { return false;}
-        return blockSpan.childNodes.length === 2 && 
-        blockSpan.children[0].tagName === "STRONG" && blockSpan.children[0].innerHTML.toUpperCase() === 'API KEY:';
+    const apiBlock = blocks.find((b) => {
+      const blockSpan = b.children[0];
+      if (!blockSpan) {
+        return false;
+      }
+      return (
+        blockSpan.childNodes.length === 2 &&
+        blockSpan.children[0].tagName === "STRONG" &&
+        blockSpan.children[0].innerHTML.toUpperCase() === "API KEY:"
+      );
     });
     const apiKey = apiBlock.children[0].childNodes[1].nodeValue;
     if (!apiKey) {
@@ -30,14 +40,20 @@ const importCalendarListener = (e: MouseEvent) => {
     }
     const timeMin = new Date();
     const timeMax = new Date();
-    const offset = timeMin.getTimezoneOffset();
-    timeMin.setHours(-offset/60, 0, 0, 0);
-    timeMax.setHours(-offset/60, 0, 0, 0);
-    timeMax.setDate(timeMax.getDate() + 1)
+    const offset = timeMin.getTimezoneOffset() / 60;
+    timeMin.setHours(-offset, 0, 0, 0);
+    timeMax.setHours(-offset, 0, 0, 0);
+    timeMax.setDate(timeMax.getDate() + 1);
+    const offsetString =
+      offset === 0 ? "Z" : `-${offset < 10 ? `0${offset}` : offset}:00`;
+    const timeMinParam = `${timeMin.toISOString()}${offsetString}`;
+    const timeMaxParam = `${timeMax.toISOString()}${offsetString}`;
 
     console.log("trigger import events");
     fetch(
-      `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(calendarId)}/events?key=${apiKey}&timeMin=${timeMin}&timeMax=${timeMax}`
+      `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(
+        calendarId
+      )}/events?key=${apiKey}&timeMin=${timeMinParam}&timeMax=${timeMaxParam}`
     )
       .then((r) => r.json())
       .then((es) => {
