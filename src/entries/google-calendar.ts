629cbd40c10856d6aead64517906ff1d9df636d6
David Vargas <dvargas92495@gmail.com>
2020-08-26 15:52:59 -0400

    Fetching Events - card-44210689 (#3)
    * Fetching Events - card-44210689

--- src/entries/google-calendar.ts
+++ src/entries/google-calendar.ts
@@ -1,1 +1,70 @@
-console.log('Google Calendar Integration coming soon!')
+const importCalendarListener = (e: MouseEvent) => {
+  const target = e.target as HTMLElement;
+  if (
+    target &&
+    target.tagName === "BUTTON" &&
+    target.innerText.toUpperCase() === "IMPORT GOOGLE CALENDAR"
+  ) {
+    const blocks = Array.from(document.getElementsByClassName("roam-block"));
+    const calendarBlock = blocks.find((b) => {
+      const blockSpan = b.children[0];
+      if (!blockSpan) {
+        return false;
+      }
+      return (
+        blockSpan.childNodes.length === 2 &&
+        blockSpan.children[0].tagName === "STRONG" &&
+        blockSpan.children[0].innerHTML.toUpperCase() === "GOOGLE CALENDAR:"
+      );
+    });
+    const calendarId = calendarBlock.children[0].childNodes[1].nodeValue;
+    if (!calendarId) {
+      console.warn("Could not find calendar ID!");
+      return;
+    }
+    const apiBlock = blocks.find((b) => {
+      const blockSpan = b.children[0];
+      if (!blockSpan) {
+        return false;
+      }
+      return (
+        blockSpan.childNodes.length === 2 &&
+        blockSpan.children[0].tagName === "STRONG" &&
+        blockSpan.children[0].innerHTML.toUpperCase() === "API KEY:"
+      );
+    });
+    const apiKey = apiBlock.children[0].childNodes[1].nodeValue;
+    if (!apiKey) {
+      console.warn("Could not find API KEY!");
+      return;
+    }
+    const timeMin = new Date();
+    const timeMax = new Date();
+    const offset = timeMin.getTimezoneOffset() / 60;
+    timeMin.setHours(-offset, 0, 0, 0);
+    timeMax.setHours(-offset, 0, 0, 0);
+    timeMax.setDate(timeMax.getDate() + 1);
+    const offsetString =
+      offset === 0 ? "Z" : `-${offset < 10 ? `0${offset}` : offset}:00`;
+    const timeMinParam = `${timeMin
+      .toISOString()
+      .substring(0, timeMin.toISOString().length - 1)}${offsetString}`;
+    const timeMaxParam = `${timeMax
+      .toISOString()
+      .substring(0, timeMin.toISOString().length - 1)}${offsetString}`;
+
+    console.log("trigger import events");
+    fetch(
+      `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(
+        calendarId
+      )}/events?key=${apiKey}&timeMin=${timeMinParam}&timeMax=${timeMaxParam}`
+    )
+      .then((r) => r.json())
+      .then((es) => {
+        console.log(es);
+        return 0;
+      });
+  }
+};
+
+document.addEventListener("click", importCalendarListener);
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const fs = require("fs");
 const path = require("path");
 
 const extensions = fs.readdirSync("./src/entries/");
-const entry = Object.fromEntries(extensions.map(e => [e.substring(0, e.length - 3), `./src/entries/${e}`]));
+const entry = Object.fromEntries(
+  extensions.map((e) => [e.substring(0, e.length - 3), `./src/entries/${e}`])
+);
 
 module.exports = {
-  target: "node",
   entry,
   resolve: {
     extensions: [".ts", ".js"],
@@ -21,6 +22,11 @@ module.exports = {
         test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
       },
     ],
   },
