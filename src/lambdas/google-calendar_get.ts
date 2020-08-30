f6b29a0cb0f152c07e9b36786a4c5436614c3abe
David Vargas <dvargas92495@gmail.com>
2020-08-30 13:18:13 -0400

    Catch lambda

--- src/lambdas/google-calendar_get.ts
+++ src/lambdas/google-calendar_get.ts
@@ -4,15 +4,27 @@ import axios from "axios";
 const apiKey = process.env.GOOGLE_CALENDAR_API_KEY;
 
 export const handler = async (event: APIGatewayProxyEvent) => {
-    const { calendarId, timeMin, timeMax } = event.queryStringParameters;
-    axios.get(`https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(
+  const { calendarId, timeMin, timeMax } = event.queryStringParameters;
+  axios
+    .get(
+      `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(
         calendarId
-      )}/events?key=${apiKey}&timeMin=${timeMin}&timeMax=${timeMax}&orderBy=startTime&singleEvents=true`).then(r => ({
-          status: 200,
-          body: JSON.stringify(r.data.items),
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-          }
-      }))
+      )}/events?key=${apiKey}&timeMin=${timeMin}&timeMax=${timeMax}&orderBy=startTime&singleEvents=true`
+    )
+    .then((r) => ({
+      status: 200,
+      body: JSON.stringify(r.data.items),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      },
+    }))
+    .catch((e) => ({
+      status: 500,
+      body: e.message,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      },
+    }));
 };
