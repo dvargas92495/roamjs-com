5ef635cfb91f82b3f15d92449b460ee36192aefc
David Vargas <dvargas92495@gmail.com>
2020-08-31 12:46:51 -0400

    Testing a risky wait

--- src/entries/google-calendar.ts
+++ src/entries/google-calendar.ts
@@ -11,10 +11,16 @@ declare global {
   }
 }
 
-// yolo wait, next character was bleeding
-// https://github.com/testing-library/user-event/blob/a5b335026abe9692a85190180603597da9687496/src/type.js#L57
-const wait = async () =>
-  await new Promise((resolve) => setTimeout(() => resolve(), 5));
+const waitUntilText = async (expected: string) => {
+  const textArea = document.activeElement as HTMLTextAreaElement;
+  let retries = 0;
+  while (textArea.value !== expected && retries < 200) {
+    // yolo wait, next character was bleeding	// yolo wait, next character was bleeding
+    // https://github.com/testing-library/user-event/blob/a5b335026abe9692a85190180603597da9687496/src/type.js#L57
+    await new Promise((resolve) => setTimeout(() => resolve(), 5));
+    retries++;
+  }
+};
 
 const slashEventListener = (e: KeyboardEvent) => {
   if (e.key !== "Enter") {
@@ -86,10 +92,10 @@ const slashEventListener = (e: KeyboardEvent) => {
         await userEvent.type(textArea, "{backspace}");
         for (const index in bullets) {
           const bullet = bullets[index];
-          await userEvent.type(document.activeElement, bullet, {
+          userEvent.type(document.activeElement, bullet, {
             skipClick: true,
           });
-          await wait();
+          waitUntilText(bullet);
 
           // Need to switch to fireEvent because user-event enters a newline when hitting enter in a text area
           // https://github.com/testing-library/user-event/blob/master/src/type.js#L505
@@ -101,7 +107,7 @@ const slashEventListener = (e: KeyboardEvent) => {
           await fireEvent.keyDown(document.activeElement, enterObj);
           await fireEvent.keyPress(document.activeElement, enterObj);
           await fireEvent.keyUp(document.activeElement, enterObj);
-          await wait();
+          waitUntilText("");
         }
         return 0;
       });
