f9063f2dea28901abc3cc2378186d2b989b96704
David Vargas <dvargas92495@gmail.com>
2020-09-01 15:28:58 -0400

    Detect emojis (#15)
    * Detect emojis
    
    * Some debugging
    
    * logic
    
    * Need emoji on

---
+++ docs/extensions/emoji.md
@@ -0,0 +1,3 @@
+## Roam Emojis
+
+The Roam Emoji extension allows users to use familiar colon syntax to insert emojis into the text block. The name of the script is `emojis`.
---
+++ src/entries/emojis.ts
@@ -0,0 +1,25 @@
+let searchText = "";
+let emojiOn = false;
+
+const turnOnEmoji = () => (emojiOn = true);
+const turnOffEmoji = () => {
+  searchText = "";
+  emojiOn = false;
+};
+
+const inputEventListener = (e: InputEvent) => {
+  if (e.data === ":") {
+    if (!emojiOn) {
+      turnOnEmoji();
+    } else {
+      console.log("GET EMOJI FOR " + searchText);
+      turnOffEmoji();
+    }
+  } else if (!/\s/.test(e.data) && e.data && emojiOn) {
+    searchText += e.data;
+  } else {
+    turnOffEmoji();
+  }
+};
+
+document.addEventListener("input", inputEventListener);
