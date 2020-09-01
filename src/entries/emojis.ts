a8f07f15c697ddadc5e9db693ee4c65289d96737
David Vargas <dvargas92495@gmail.com>
2020-09-01 16:08:25 -0400

    Replace emojis with actual code

--- src/entries/emojis.ts
+++ src/entries/emojis.ts
@@ -1,3 +1,7 @@
+import emoji from "node-emoji";
+import userEvent from "@testing-library/user-event";
+import { waitFor } from "@testing-library/dom";
+
 let searchText = "";
 let emojiOn = false;
 
@@ -7,12 +11,28 @@ const turnOffEmoji = () => {
   emojiOn = false;
 };
 
-const inputEventListener = (e: InputEvent) => {
+const waitForCallback = (text: string) => () => {
+  const textArea = document.activeElement as HTMLTextAreaElement;
+  if (textArea.value.toUpperCase() !== text.toUpperCase()) {
+    throw new Error("Typing not complete");
+  }
+};
+
+const inputEventListener = async (e: InputEvent) => {
   if (e.data === ":") {
     if (!emojiOn) {
       turnOnEmoji();
     } else {
-      console.log("GET EMOJI FOR " + searchText);
+      const emojiCode = emoji.get(searchText);
+      const target = e.target as HTMLTextAreaElement;
+      const initialValue = target.value;
+      const preValue = initialValue.substring(
+        0,
+        initialValue.length - 2 - searchText.length
+      );
+      target.setSelectionRange(preValue.length, initialValue.length);
+      userEvent.type(target, "{backspace}");
+      userEvent.type(target, emojiCode);
       turnOffEmoji();
     }
   } else if (!/\s/.test(e.data) && e.data && emojiOn) {
