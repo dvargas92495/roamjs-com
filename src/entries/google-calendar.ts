31488165df50e1b7c935fa7daf3d79a557102eaf
David Vargas <dvargas92495@gmail.com>
2020-09-02 13:33:42 -0400

    Closes #20

--- src/entries/google-calendar.ts
+++ src/entries/google-calendar.ts
@@ -23,81 +23,90 @@ const waitForCallback = (text: string) => () => {
   }
 };
 
-const importGoogleCalendar = () => {
-  const configurationAttrRefs = window.roamAlphaAPI
-  .q(
+const importGoogleCalendar = async () => {
+  const pageResults = window.roamAlphaAPI.q(
     '[:find (pull ?e [*]) :where [?e :node/title "roam/js/google-calendar"] ]'
-  )[0][0]
-  .attrs.map((a: any) => a[2].source[1]);
-const entries = configurationAttrRefs.map((r: string) =>
-  window.roamAlphaAPI
-    .q(
-      `[:find (pull ?e [:block/string]) :where [?e :block/uid "${r}"] ]`
-    )[0][0]
-    .string.split("::")
-);
-const config = Object.fromEntries(entries);
+  );
+  if (pageResults.length === 0) {
+    await asyncType("Error: Could not find the [[roam/js/google-calendar]] page. Please add this page and configure the Google Calendar attribute");
+    return;
+  }
 
-const calendarId = config["Google Calendar"];
-if (!calendarId) {
-  console.warn("Could not find calendar ID!");
-  return;
-}
-const includeLink = config["Include Event Link"] === 'true';
-const timeMin = new Date();
-const timeMax = new Date();
-const offset = timeMin.getTimezoneOffset() / 60;
-timeMin.setHours(-offset, 0, 0, 0);
-timeMax.setHours(-offset, 0, 0, 0);
-timeMax.setDate(timeMax.getDate() + 1);
-const offsetString =
-  offset === 0 ? "Z" : `-${offset < 10 ? `0${offset}` : offset}:00`;
-const timeMinParam = `${timeMin
-  .toISOString()
-  .substring(0, timeMin.toISOString().length - 1)}${offsetString}`;
-const timeMaxParam = `${timeMax
-  .toISOString()
-  .substring(0, timeMin.toISOString().length - 1)}${offsetString}`;
+  const configurationAttrRefs = pageResults[0][0].attrs.map(
+    (a: any) => a[2].source[1]
+  );
+  const entries = configurationAttrRefs.map((r: string) =>
+    window.roamAlphaAPI
+      .q(
+        `[:find (pull ?e [:block/string]) :where [?e :block/uid "${r}"] ]`
+      )[0][0]
+      .string.split("::")
+  );
+  const config = Object.fromEntries(entries);
 
-fetch(
-  `https://12cnhscxfe.execute-api.us-east-1.amazonaws.com/production/google-calendar?calendarId=${calendarId}&timeMin=${timeMinParam}&timeMax=${timeMaxParam}`
-)
-  .then((r) => r.json())
-  .then(async (r) => {
-    const events = r.items;
-    const bullets = events.map((e: any) => {
-      const summary = includeLink && e.htmlLink ? `[${e.summary}](${e.htmlLink})` : e.summary;
-      const meetLink = e.hangoutLink ? ` - [Meet](${e.hangoutLink})` : "";
-      const zoomLink =
-        e.location && e.location.indexOf("zoom.us") > -1
-          ? ` - [Zoom](${e.location})`
-          : "";
-      return `${summary} (${new Date(
-        e.start.dateTime
-      ).toLocaleTimeString()} - ${new Date(
-        e.end.dateTime
-      ).toLocaleTimeString()})${meetLink}${zoomLink}`;
-    }) as string[];
-    for (const index in bullets) {
-      const bullet = bullets[index];
-      await asyncType(bullet);
-      await waitFor(waitForCallback(bullet));
+  const calendarId = config["Google Calendar"];
+  if (!calendarId) {
+    console.warn("Could not find calendar ID!");
+    return;
+  }
+  const includeLink = config["Include Event Link"] === "true";
+  const timeMin = new Date();
+  const timeMax = new Date();
+  const offset = timeMin.getTimezoneOffset() / 60;
+  timeMin.setHours(-offset, 0, 0, 0);
+  timeMax.setHours(-offset, 0, 0, 0);
+  timeMax.setDate(timeMax.getDate() + 1);
+  const offsetString =
+    offset === 0 ? "Z" : `-${offset < 10 ? `0${offset}` : offset}:00`;
+  const timeMinParam = `${timeMin
+    .toISOString()
+    .substring(0, timeMin.toISOString().length - 1)}${offsetString}`;
+  const timeMaxParam = `${timeMax
+    .toISOString()
+    .substring(0, timeMin.toISOString().length - 1)}${offsetString}`;
 
-      // Need to switch to fireEvent because user-event enters a newline when hitting enter in a text area
-      // https://github.com/testing-library/user-event/blob/master/src/type.js#L505
-      const enterObj = {
-        key: "Enter",
-        keyCode: 13,
-        which: 13,
-      };
-      await fireEvent.keyDown(document.activeElement, enterObj);
-      await fireEvent.keyPress(document.activeElement, enterObj);
-      await fireEvent.keyUp(document.activeElement, enterObj);
-      await waitFor(waitForCallback(""));
-    }
-    return 0;
-  });
-}
+  fetch(
+    `https://12cnhscxfe.execute-api.us-east-1.amazonaws.com/production/google-calendar?calendarId=${calendarId}&timeMin=${timeMinParam}&timeMax=${timeMaxParam}`
+  )
+    .then((r) => r.json())
+    .then(async (r) => {
+      const events = r.items;
+      const bullets = events.map((e: any) => {
+        const summary =
+          includeLink && e.htmlLink
+            ? `[${e.summary}](${e.htmlLink})`
+            : e.summary;
+        const meetLink = e.hangoutLink ? ` - [Meet](${e.hangoutLink})` : "";
+        const zoomLink =
+          e.location && e.location.indexOf("zoom.us") > -1
+            ? ` - [Zoom](${e.location})`
+            : "";
+        return `${summary} (${new Date(
+          e.start.dateTime
+        ).toLocaleTimeString()} - ${new Date(
+          e.end.dateTime
+        ).toLocaleTimeString()})${meetLink}${zoomLink}`;
+      }) as string[];
+      for (const index in bullets) {
+        const bullet = bullets[index];
+        await asyncType(bullet);
+        await waitFor(waitForCallback(bullet));
+
+        // Need to switch to fireEvent because user-event enters a newline when hitting enter in a text area
+        // https://github.com/testing-library/user-event/blob/master/src/type.js#L505
+        const enterObj = {
+          key: "Enter",
+          keyCode: 13,
+          which: 13,
+        };
+        await fireEvent.keyDown(document.activeElement, enterObj);
+        await fireEvent.keyPress(document.activeElement, enterObj);
+        await fireEvent.keyUp(document.activeElement, enterObj);
+        await waitFor(waitForCallback(""));
+      }
+      return 0;
+    });
+};
 
 const clickEventListener = async (e: MouseEvent) => {
   const target = e.target as HTMLElement;
@@ -106,7 +115,8 @@ const clickEventListener = async (e: MouseEvent) => {
     target.tagName === "BUTTON" &&
     target.innerText.toUpperCase() === GOOGLE_COMMAND.toUpperCase()
   ) {
-    const divContainer = target.parentElement.parentElement.parentElement as HTMLDivElement;
+    const divContainer = target.parentElement.parentElement
+      .parentElement as HTMLDivElement;
     await userEvent.click(divContainer);
     await waitFor(waitForCallback(`{{${GOOGLE_COMMAND}}}`));
     const textArea = document.activeElement as HTMLTextAreaElement;
@@ -114,6 +124,6 @@ const clickEventListener = async (e: MouseEvent) => {
     await waitFor(waitForCallback(""));
     importGoogleCalendar();
   }
-}
+};
 
 document.addEventListener("click", clickEventListener);
