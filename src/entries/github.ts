4abfea83109e91f8a48c902ba164372b33fbd476
David Vargas <dvargas92495@gmail.com>
2020-09-07 14:25:16 -0400

    Issues not cards

--- src/entries/github.ts
+++ src/entries/github.ts
@@ -2,7 +2,7 @@ import { addButtonListener } from "../entry-helpers";
 
 const importGithubIssues = () => {
   fetch(
-    `https://12cnhscxfe.execute-api.us-east-1.amazonaws.com/production/github-cards`
+    `https://12cnhscxfe.execute-api.us-east-1.amazonaws.com/production/github-issues`
   );
 };
 
