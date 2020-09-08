2c52e6bcaf62672dcfea1f08057726d2e8647607
David Vargas <dvargas92495@gmail.com>
2020-09-08 13:09:12 -0400

    Wrong path

--- src/entries/github.ts
+++ src/entries/github.ts
@@ -62,7 +62,7 @@ const importGithubRepos = async (buttonConfig: { [key: string]: string }) => {
         },
       })
     : fetch(
-        `https://12cnhscxfe.execute-api.us-east-1.amazonaws.com/production/github-repos?username=${username}`
+        `https://12cnhscxfe.execute-api.us-east-1.amazonaws.com/production/github-repositories?username=${username}`
       );
   githubFetch
     .then((r) => {
