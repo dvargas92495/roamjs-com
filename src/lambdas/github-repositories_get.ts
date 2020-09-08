3b0dbcab1e28ed34f81c8e7265a5dda13db8a5cc
David Vargas <dvargas92495@gmail.com>
2020-09-08 12:08:02 -0400

    Repo get lambda

--- src/lambdas/github-repositories_get.ts
+++ src/lambdas/github-repositories_get.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
-import { wrapAxios, getGithubOpts } from "../lambda-helpers";
+import { wrapAxios, getGithubOpts, userError } from "../lambda-helpers";
+import { APIGatewayEvent } from "aws-lambda";
 
-export const handler = async () => {
+export const handler = async (event: APIGatewayEvent) => {
+  const { username } = event.queryStringParameters;
+  if (!username) {
+    return userError("username is required");
+  }
   const opts = getGithubOpts();
   return wrapAxios(
-    axios(`https://api.github.com/repos/orgs/dvargas92495/repos`, opts)
+    axios(`https://api.github.com/repos/users/${username}/repos`, opts)
   );
 };
