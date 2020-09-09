34a87404b27276ab5e2b59a2f438160c780d5b20
David Vargas <dvargas92495@gmail.com>
2020-09-09 16:25:22 -0400

    Button querying Github cards (Closes #42) (#53)
    * Button querying Github cards (Closes #42)
    
    * Crazy defaults
    
    * fix project
    
    * Skipping cards token for now
    
    * Fix and docs

--- src/entries/github.ts
+++ src/entries/github.ts
@@ -56,9 +56,9 @@ const importGithubRepos = async (buttonConfig: { [key: string]: string }) => {
   const githubFetch = token
     ? fetch(`https://api.github.com/users/${username}/repos`, {
         headers: {
-          Authorization: `Basic ${Buffer.from(`${config["Username"]}:${token}`).toString(
-            "base64"
-          )}`,
+          Authorization: `Basic ${Buffer.from(
+            `${config["Username"]}:${token}`
+          ).toString("base64")}`,
         },
       })
     : fetch(
@@ -99,9 +99,10 @@ const importGithubProjects = async (buttonConfig: {
   const githubFetch = token
     ? fetch(`https://api.github.com/repos/${repository}/projects`, {
         headers: {
-          Authorization: `Basic ${Buffer.from(`${config["Username"]}:${token}`).toString(
-            "base64"
-          )}`,
+          Authorization: `Basic ${Buffer.from(
+            `${config["Username"]}:${token}`
+          ).toString("base64")}`,
+          Accept: "application/vnd.github.inertia-preview+json",
         },
       })
     : fetch(
@@ -128,7 +129,52 @@ const importGithubProjects = async (buttonConfig: {
     .catch((e) => asyncType(`Error: ${e.message}`));
 };
 
-addButtonListener("Import Github Cards", () => {});
+const importGithubCards = async (buttonConfig: { [key: string]: string }) => {
+  const config = getConfigFromPage("github");
+  const pageTitle = document.getElementsByClassName(
+    "rm-title-display"
+  )[0] as HTMLHeadingElement;
+  const parentBlocks = window.roamAlphaAPI
+    .q(
+      `[:find (pull ?parentPage [:node/title]) :where [?parentPage :block/children ?referencingBlock] [?referencingBlock :block/refs ?referencedPage] [?referencedPage :node/title "${pageTitle.innerText}"]]`
+    )
+    .filter((block) => block.length);
+  const repoAsParent = parentBlocks.length > 0 ? parentBlocks[0][0]?.title : "";
+
+  const username = buttonConfig.FOR ? buttonConfig.FOR : config["Username"];
+  const repoName = buttonConfig.IN ? buttonConfig.IN : repoAsParent;
+  const repository = `${username}/${repoName}`;
+  const project = buttonConfig.UNDER ? buttonConfig.UNDER : pageTitle.innerText;
+  const column = buttonConfig.AS ? buttonConfig.AS : "To do";
+
+  if (!config["Token"]) {
+    fetch(
+      `https://12cnhscxfe.execute-api.us-east-1.amazonaws.com/production/github-cards?repository=${repository}&project=${project}&column=${column}`
+    )
+      .then((r) => {
+        if (!r.ok) {
+          return r
+            .text()
+            .then((errorMessage) =>
+              asyncType(`Error fetching cards: ${errorMessage}`)
+            );
+        }
+        return r.json().then(async (cards) => {
+          if (cards.length === 0) {
+            await asyncType(`No cards in ${repository}`);
+            return;
+          }
+          const bullets = cards.map((i: any) => `[${i.id}](${i.html_link})`);
+          await pushBullets(bullets);
+        });
+      })
+      .catch((e) => asyncType(`Error: ${e.message}`));
+  } else {
+    await asyncType("Personal Token currently not supported for cards");
+  }
+};
+
+addButtonListener("Import Github Cards", importGithubCards);
 addButtonListener("Import Github Issues", importGithubIssues);
 addButtonListener("Import Github Projects", importGithubProjects);
 addButtonListener("Import Github Repos", importGithubRepos);
