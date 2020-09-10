a566bcf1dfb578f74d001f17473ce68038d94746
David Vargas <dvargas92495@gmail.com>
2020-09-10 19:00:37 -0400

    Demo video

--- src/entries/github.ts
+++ src/entries/github.ts
@@ -164,7 +164,7 @@ const importGithubCards = async (buttonConfig: { [key: string]: string }) => {
             await asyncType(`No cards in ${repository}`);
             return;
           }
-          const bullets = cards.map((i: any) => i.note ? i.note : i.content_url);
+          const bullets = cards.map((i: any) => i.note ? i.note : i.content_url.substring("https://api.github.com".length));
           await pushBullets(bullets);
         });
       })
