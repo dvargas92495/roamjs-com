69828c7af1607b7ec0b648781f5bcb6f29666692
David Vargas <dvargas92495@gmail.com>
2020-09-11 13:28:45 -0400

    Important hack

--- src/entries/todont.ts
+++ src/entries/todont.ts
@@ -1,23 +1,28 @@
 import userEvent from "@testing-library/user-event";
 import { asyncType } from "../entry-helpers";
 
-const mutationConfig = { childList: true, subtree: true};
-const mutationTarget = document.getElementsByClassName('roam-article')[0];
+const styleArchivedButtons = (node: HTMLElement) => {
+  const buttons = node.getElementsByTagName("button");
+  Array.from(buttons).forEach((button) => {
+    if (button.innerText === "ARCHIVED") {
+      button.style.cssText = "background-color:red !important";
+      button.innerText = "x";
+      button.style.borderRadius = "0";
+      button.style.padding = "0";
+      button.style.minHeight = "0";
+      button.style.minWidth = "0";
+    }
+  });
+};
+styleArchivedButtons(document.body);
+
+const mutationConfig = { childList: true, subtree: true };
+const mutationTarget = document.getElementsByClassName("roam-article")[0];
 const mutationCallback = (mutationList: MutationRecord[]) => {
-    mutationList.forEach(record => {
-        const node = record.target as HTMLElement;
-        const buttons = node.getElementsByTagName('button');
-        Array.from(buttons).forEach(button => {
-            console.log(button.innerText);
-            if (button.innerText === 'ARCHIVED') {
-                button.style.backgroundColor = 'red !important';
-                button.innerText = 'x';
-                button.style.borderRadius = '0';
-                button.style.padding = '0';
-            }
-        })
-    })
-}
+  mutationList.forEach((record) => {
+    styleArchivedButtons(record.target as HTMLElement);
+  });
+};
 const observer = new MutationObserver(mutationCallback);
 observer.observe(mutationTarget, mutationConfig);
 
