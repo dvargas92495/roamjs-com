a7a491c054fade705bcc2832cae87a085d385790
David Vargas <dvargas92495@gmail.com>
2020-09-14 12:29:22 -0400

    Button On click

--- src/entries/sort-references.ts
+++ src/entries/sort-references.ts
@@ -2,6 +2,7 @@ const referenceButtonContainer = document.getElementsByClassName(
   "rm-reference-container dont-focus-block"
 )[0] as HTMLDivElement;
 
+// Icon Button
 const popoverWrapper = document.createElement("span");
 popoverWrapper.className = "bp3-popover-wrapper";
 referenceButtonContainer.appendChild(popoverWrapper);
@@ -10,10 +11,6 @@ const popoverTarget = document.createElement("span");
 popoverTarget.className = "bp3-popover-target";
 popoverWrapper.appendChild(popoverTarget);
 
-const popoverOverlay = document.createElement("div");
-popoverOverlay.className = "bp3-overlay bp3-overlay-inline";
-popoverWrapper.appendChild(popoverOverlay);
-
 const popoverButton = document.createElement("span");
 popoverButton.className = "bp3-button bp3-minimal bp3-small";
 popoverButton.tabIndex = 0;
@@ -21,4 +18,47 @@ popoverTarget.appendChild(popoverButton);
 
 const popoverIcon = document.createElement("span");
 popoverIcon.className = "bp3-icon bp3-icon-sort";
-popoverTarget.appendChild(popoverIcon);
+popoverButton.appendChild(popoverIcon);
+
+// Overlay Content
+const popoverOverlay = document.createElement("div");
+popoverOverlay.className = "bp3-overlay bp3-overlay-inline";
+popoverWrapper.appendChild(popoverOverlay);
+
+const transitionContainer = document.createElement("div");
+transitionContainer.className =
+  "bp3-transition-container bp3-popover-enter-done";
+transitionContainer.style.position = "absolute";
+transitionContainer.style.willChange = "transform";
+transitionContainer.style.top = "0";
+transitionContainer.style.left = "0";
+transitionContainer.style.transform = "translate3d(723px, 50px, 0px);";
+
+const popover = document.createElement("div");
+popover.className = "bp3-popover";
+popover.style.transformOrigin = "162px top";
+transitionContainer.appendChild(popover);
+
+const popoverContent = document.createElement("div");
+popoverContent.className = "bp3-popover-content";
+popover.appendChild(popoverContent);
+
+const menuUl = document.createElement("ul");
+menuUl.className = "bp3-menu";
+popoverContent.appendChild(menuUl);
+
+const createMenuItem = (text: string) => {
+  const liItem = document.createElement("li");
+  const aMenuItem = document.createElement("a");
+  aMenuItem.className = "bp3-menu-item bp3-popover-dismiss";
+  liItem.appendChild(aMenuItem);
+  const menuItemText = document.createElement("div");
+  menuItemText.className = "bp3-text-overflow-ellipsis bp3-fill";
+  menuItemText.innerText = text;
+  aMenuItem.appendChild(menuItemText);
+  menuUl.appendChild(liItem);
+};
+createMenuItem("Sort By Title");
+createMenuItem("Sort By Created Date");
+
+popoverButton.onclick = () => popoverOverlay.appendChild(transitionContainer);
