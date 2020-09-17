990727fca9af2436ad44e4b749d44b02c794fa10
David Vargas <dvargas92495@gmail.com>
2020-09-17 13:43:26 -0400

    Add More Mobile Icons (Closes #91)

---
+++ src/entries/mobile-todos.ts
@@ -0,0 +1,6 @@
+import userEvent from "@testing-library/user-event";
+
+document.addEventListener("input", () => {
+    const text = document.body.innerHTML;
+    userEvent.type(document.activeElement, text);
+})
