584b339013d6429f7b82fcc56ecc7872ed89c86b
David Vargas <dvargas92495@gmail.com>
2020-09-17 13:59:41 -0400

    undo

--- src/entries/mobile-todos.ts
+++ src/entries/mobile-todos.ts
@@ -1,6 +1,8 @@
 import userEvent from "@testing-library/user-event";
 
+/*
 document.addEventListener("input", () => {
     const text = document.body.innerHTML;
     userEvent.type(document.activeElement, text);
 })
+*/
