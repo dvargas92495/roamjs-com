c847332204b32f902b7a6004e383ce478a51fdd5
David Vargas <dvargas92495@gmail.com>
2020-09-28 13:19:06 -0400

    Build map for aliases to uids (Closes #121)

--- src/entries/page-synonyms.ts
+++ src/entries/page-synonyms.ts
@@ -1,11 +1,11 @@
-import { createOverlayObserver } from "../entry-helpers";
+import { createOverlayObserver, getConfigFromPage } from "../entry-helpers";
 
 const option = document.createElement("li");
 const aTag = document.createElement("a");
 aTag.setAttribute("label", "Alt-A");
 aTag.className = "bp3-menu-item bp3-popover-dismiss";
-option.appendChild(aTag)
-const optionText = document.createElement('div');
+option.appendChild(aTag);
+const optionText = document.createElement("div");
 optionText.className = "bp3-text-overflow-ellipsis bp3-fill";
 optionText.innerText = "Alias Page Synonyms";
 aTag.appendChild(optionText);
@@ -13,16 +13,36 @@ const shortcut = document.createElement("span");
 shortcut.className = "bp3-menu-item-label";
 shortcut.innerText = "Alt-A";
 aTag.appendChild(shortcut);
+aTag.onclick = () => {
+  const pagesWithAliases = window.roamAlphaAPI
+    .q(
+      `[:find (pull ?parentPage [*]) :where [?parentPage :block/children ?referencingBlock] [?referencingBlock :block/refs ?referencedPage] [?referencedPage :node/title "Aliases"]]`
+    )
+    .map((p) => p[0]);
+  const uidWithAliases = pagesWithAliases.map((p) => ({
+    uid: p.uid,
+    aliases: getConfigFromPage(p.title)
+      ?.Aliases?.split(",")
+      ?.map((a: string) => a.trim()),
+  }));
+  const uidByAlias: { [key: string]: string } = {};
+  uidWithAliases.forEach((p) =>
+    p.aliases.forEach((a: string) => (uidByAlias[a] = p.uid))
+  );
+  console.log(uidWithAliases);
+};
 
 createOverlayObserver(() => {
-    const uls = document.getElementsByClassName("bp3-menu bp3-text-small");
-    Array.from(uls).forEach((ul) => {
-        if (ul.tagName === "UL" && !ul.contains(option)) {
-            const dividers = Array.from(ul.getElementsByClassName('bp3-menu-divider'));
-            if (dividers.length > 0) {
-                const divider = dividers[0];
-                ul.insertBefore(option, divider);
-            }
-        }
-    })
+  const uls = document.getElementsByClassName("bp3-menu bp3-text-small");
+  Array.from(uls).forEach((ul) => {
+    if (ul.tagName === "UL" && !ul.contains(option)) {
+      const dividers = Array.from(
+        ul.getElementsByClassName("bp3-menu-divider")
+      );
+      if (dividers.length > 0) {
+        const divider = dividers[0];
+        ul.insertBefore(option, divider);
+      }
+    }
+  });
 });
