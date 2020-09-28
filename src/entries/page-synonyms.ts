9a02734ce092d9f3c1af8c86545f070b086eaf56
David Vargas <dvargas92495@gmail.com>
2020-09-28 12:45:35 -0400

    Found the typo

--- src/entries/page-synonyms.ts
+++ src/entries/page-synonyms.ts
@@ -15,14 +15,10 @@ shortcut.innerText = "Alt-A";
 aTag.appendChild(shortcut);
 
 createOverlayObserver(() => {
-    console.log("observing");
     const uls = document.getElementsByClassName("bp3-menu bp3-text-small");
-    console.log(uls);
     Array.from(uls).forEach((ul) => {
-        console.log(ul.tagName);
         if (ul.tagName === "UL") {
-            const dividers = Array.from(ul.getElementsByClassName('bp3-menu-divieder'));
-            console.log(dividers);
+            const dividers = Array.from(ul.getElementsByClassName('bp3-menu-divider'));
             if (dividers.length > 0) {
                 const divider = dividers[0];
                 ul.insertBefore(option, divider);
