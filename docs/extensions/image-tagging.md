53f2bd2ae539a8f5bb815a379a586fcdec21d923
David Vargas <dvargas92495@gmail.com>
2020-10-01 14:05:23 -0400

    Import Tesseract (Closes #143) (#148)
    * Import Tesseract (Closes #143)
    
    * Dumb CORS
    
    * Try with context
    
    * Let's try create worker
    
    * Try img again
    
    * keep ctx
    
    * Use canvas instead of ctx
    
    * Create Worker in callback

--- docs/extensions/image-tagging.md
+++ docs/extensions/image-tagging.md
@@ -2,8 +2,8 @@
 
 ## Image Tagging
 
-The Image Tagging extensions generates roam pages based on the content of the image!
+The Image Tagging extension extracts the text from an image and outputs it into Roam!
 
 ## Usage
 
-Double click on an image in your database.
+Double click on an image in your database. The extension will use an OCR library to extract all the text found in an image.
--- src/entries/image-tagging.ts
+++ src/entries/image-tagging.ts
@@ -1,6 +1,29 @@
+import { createWorker } from "tesseract.js";
+
 document.addEventListener("dblclick", (e) => {
-    const htmlTarget = e.target as HTMLElement;
-    if (htmlTarget && htmlTarget.tagName === 'IMG') {
-        console.log("nice");
-    }
+  const htmlTarget = e.target as HTMLElement;
+  if (htmlTarget && htmlTarget.tagName === "IMG") {
+    const img = htmlTarget as HTMLImageElement;
+
+    const tesseractImage = document.createElement("img");
+    tesseractImage.src = img.src;
+    tesseractImage.crossOrigin = "Anonymous";
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    tesseractImage.onload = async () => {
+      canvas.width = tesseractImage.width;
+      canvas.height = tesseractImage.height;
+      ctx.drawImage(tesseractImage, 0, 0);
+      const worker = createWorker();
+      await worker.load();
+      await worker.loadLanguage("eng");
+      await worker.initialize("eng");
+      const {
+        data: { text },
+      } = await worker.recognize(canvas);
+      console.log(text);
+      await worker.terminate();
+    };
+  }
 });
