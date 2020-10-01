36710dd72409ece97710de45bf2f3d3b942d0005
David Vargas <dvargas92495@gmail.com>
2020-10-01 13:22:55 -0400

    Try with context

--- src/entries/image-tagging.ts
+++ src/entries/image-tagging.ts
@@ -1,14 +1,23 @@
-import Tesseract from 'tesseract.js';
+import Tesseract from "tesseract.js";
 
 document.addEventListener("dblclick", (e) => {
-    const htmlTarget = e.target as HTMLElement;
-    if (htmlTarget && htmlTarget.tagName === 'IMG') {
-        const img = htmlTarget as HTMLImageElement;
-        const tesseractImage = document.createElement('img');
-        tesseractImage.src = img.src;
-        tesseractImage.crossOrigin = 'Anonymous';
-        Tesseract.recognize(tesseractImage, 'eng').then(({ data: {text}}) => {
-            console.log(text);
-        })
-    }
+  const htmlTarget = e.target as HTMLElement;
+  if (htmlTarget && htmlTarget.tagName === "IMG") {
+    const img = htmlTarget as HTMLImageElement;
+
+    const tesseractImage = document.createElement("img");
+    tesseractImage.src = img.src;
+    tesseractImage.crossOrigin = "Anonymous";
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    tesseractImage.onload = () => {
+      canvas.width = tesseractImage.width;
+      canvas.height = tesseractImage.height;
+      ctx.drawImage(tesseractImage, 0, 0);
+      Tesseract.recognize(ctx).then(({ data: { text } }) => {
+        console.log(text);
+      });
+    };
+  }
 });
