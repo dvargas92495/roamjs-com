322dd97534ae5c69504217d4ef73c7cb499d4ae5
David Vargas <dvargas92495@gmail.com>
2020-10-01 12:52:27 -0400

    Import Tesseract (Closes #143)

--- src/entries/image-tagging.ts
+++ src/entries/image-tagging.ts
@@ -1,6 +1,11 @@
+import Tesseract from 'tesseract.js';
+
 document.addEventListener("dblclick", (e) => {
     const htmlTarget = e.target as HTMLElement;
     if (htmlTarget && htmlTarget.tagName === 'IMG') {
-        console.log("nice");
+        const img = htmlTarget as HTMLImageElement;
+        Tesseract.recognize(img.src, 'eng').then(({ data: {text}}) => {
+            console.log(text);
+        })
     }
 });
