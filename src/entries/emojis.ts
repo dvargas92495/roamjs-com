d8285587dedfa0c0584681eeb69ed0aaa1a40df3
David Vargas <dvargas92495@gmail.com>
2020-10-05 14:15:07 -0400

    Fix config name

--- src/entries/emojis.ts
+++ src/entries/emojis.ts
@@ -2,7 +2,7 @@ import emoji from "node-emoji";
 import userEvent from "@testing-library/user-event";
 import { getConfigFromPage } from "../entry-helpers";
 
-const config = getConfigFromPage("emoji");
+const config = getConfigFromPage("emojis");
 const minimumCharacters = config["Minimum Characters"]
   ? parseInt(config["Minimum Characters"])
   : 2;
