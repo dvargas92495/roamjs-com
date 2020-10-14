8137acca5f58954242b8b811b28583f123dd9755
David Vargas <dvargas92495@gmail.com>
2020-10-13 22:10:37 -0400

    Pushing automation

--- src/automations/template-daily-note.ts
+++ src/automations/template-daily-note.ts
@@ -1,1 +1,15 @@
-export const handler = () => {};
+import format from "date-fns/format";
+import { RestClient } from "roam-client";
+
+const client = new RestClient({
+  graphName: "roam-js-extensions",
+});
+
+export const handler = async () => {
+  const parentUid = format(new Date(), "MM-dd-yyyy");
+  await client.createBlock({
+    parentUid,
+    order: 1,
+    text: "Testing Automations!",
+  });
+};
