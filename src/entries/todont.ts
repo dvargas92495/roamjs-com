2e67a9ddac637cc5c83cda781de78cea89c7e7ef
David Vargas <dvargas92495@gmail.com>
2020-10-14 13:29:50 -0400

    text content instead of innertext

--- src/entries/todont.ts
+++ src/entries/todont.ts
@@ -6,7 +6,7 @@ const TODONT_CLASSNAME = "roamjs-todont";
 const css = document.createElement("link");
 css.type = "text/css";
 css.rel = "stylesheet";
-css.innerText = `.${TODONT_CLASSNAME} {\n    background-color: red !important;\n    borderRadius: 0;\n    padding: 0;\n    minHeight: 0;\n    minWidth: 0;\n    height: 16px;\n}`;
+css.textContent = `.${TODONT_CLASSNAME} {\n    background-color: red !important;\n    borderRadius: 0;\n    padding: 0;\n    minHeight: 0;\n    minWidth: 0;\n    height: 16px;\n}`;
 document.getElementsByTagName('head')[0].appendChild(css);
 
 const styleArchivedButtons = (node: HTMLElement) => {
