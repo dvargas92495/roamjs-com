ff7bfb1a2902e0d5fee8a91593bfddfe5638fcf3
David Vargas <dvargas92495@gmail.com>
2020-10-16 15:55:49 -0400

    Daily Note Templating

--- src/automations/template-daily-note.ts
+++ src/automations/template-daily-note.ts
@@ -1,16 +1,35 @@
-import format from "date-fns/format";
-import { RestClient } from "roam-client";
+import { RestClient, toRoamDate, toRoamDateUid } from "roam-client";
 
 const client = new RestClient({
   graphName: "roam-js-extensions",
 });
 
 export const handler = async () => {
-  const parentUid = format(new Date(), "MM-dd-yyyy");
-  const output = await client.createBlock({
-    parentUid,
-    order: 1,
-    text: "Testing Automations!",
-  });
-  console.log(output);
+  try {
+    const today = new Date();
+    const title = toRoamDate(today);
+    const parentUid = toRoamDateUid(today);
+    const queryResults = await client.q({
+      query: `[:find ?e :where [?e :node/title "${title}"]]`,
+    });
+    if (queryResults.length === 0) {
+      await client.createPage({
+        title,
+        uid: parentUid,
+      });
+    }
+    const template = await client.q({
+      query: `[:find (pull ?c [:block/string :block/order]) :where [?page :block/children ?c] [?page :node/title "roam/js/template-daily-note"]]`,
+    });
+    template.sort((a, b) => a["block/order"] - b["block/order"]);
+    for (var b = 0; b < template.length; b++) {
+      await client.createBlock({
+        parentUid,
+        order: template[b]["block/order"],
+        text: template[b]["block/string"],
+      });
+    }
+  } catch (e) {
+    console.error(e.response.data.error);
+  }
 };
