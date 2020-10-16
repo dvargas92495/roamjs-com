0d0a58362daa8d89e711dec21f6e6be37e4d3025
David Vargas <dvargas92495@gmail.com>
2020-10-15 20:31:35 -0400

    Add Automations Page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <AppBar homeIcon={<RoamJSLogo size={2} />} pages={["about", "docs"]} />
+    <AppBar homeIcon={<RoamJSLogo size={2} />} pages={["about", "docs", "automations"]} />
     <Main>
       {children}
     </Main>
---
+++ pages/automations/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+const AutomationsPage = () => (
+  <Layout>
+    <h1>
+      Automations - <i>Coming Soon</i>
+    </h1>
+    <p>
+      Once Roam's new backend API is live, you will be able to use this page to
+      configure automations. Until then, this page will serve as a sneak peak as
+      to what's coming!
+    </p>
+    <ul>
+        <li>
+            Template Daily Notes
+        </li>
+    </ul>
+  </Layout>
+);
+
+export default AutomationsPage;
