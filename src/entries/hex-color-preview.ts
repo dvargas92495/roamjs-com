f317c8c8683eed70cad157ca54bc403ce54ecde8
David Vargas <dvargas92495@gmail.com>
2020-10-19 12:54:18 -0400

    Detecting valid colors (Closes #214)

--- src/entries/hex-color-preview.ts
+++ src/entries/hex-color-preview.ts
@@ -1,5 +1,27 @@
-import { createObserver } from "../entry-helpers";
+import { createObserver, getRefTitlesByBlockUid, getUids } from "../entry-helpers";
+import Color from 'color';
 
 createObserver((ms) => {
+  const record = ms.find(
+    (m) =>
+      !!Array.from(m.addedNodes).find(
+        (d) =>
+          d.nodeName === "DIV" &&
+          (d as HTMLDivElement).className.indexOf("roam-block") > -1
+      )
+  );
+  if (record) {
+    const block = record.addedNodes[0] as HTMLDivElement;
+    const { blockUid } = getUids(block);
+    const refs = getRefTitlesByBlockUid(blockUid);
+    refs.forEach(r => {
+        try{
+            const c = Color(`#${r}`);
+            console.log("Successfully parsed color!", c);
+        } catch(e) {
+            console.error(e);
+        }
+    })
+  }
   console.log(ms);
 });
