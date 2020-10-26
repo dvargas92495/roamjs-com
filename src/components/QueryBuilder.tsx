b5e0b35de24be4da6ed1710a127386160836eb5f
David Vargas <dvargas92495@gmail.com>
2020-10-26 15:19:56 -0400

    Top level queries

--- src/components/QueryBuilder.tsx
+++ src/components/QueryBuilder.tsx
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Button, Popover } from "@blueprintjs/core";
+import { Select } from "@blueprintjs/select";
+import { asyncType, openBlock } from "roam-client";
 
-const QueryBuilder = () => {
+enum NODES {
+  OR,
+  AND,
+  NOT,
+  BETWEEN,
+  TAG,
+}
+
+const NodeSelect = Select.ofType<NODES>();
+
+type QueryState = {
+  type: NODES;
+  children: QueryState[];
+};
+
+const toQueryString = (queryState: QueryState) => {
+  const operator = queryState.type.toLocaleString().toLowerCase();
+  return `${operator}:{}`;
+};
+
+const QueryContent = ({ blockId }: { blockId: string }) => {
+  const [queryState, setQueryState] = useState<QueryState>({
+    type: NODES.OR,
+    children: [],
+  });
+  const onSave = useCallback(async () => {
+    const outputText = `{{[[query]]: {${toQueryString(queryState)}}}}`;
+    await openBlock(document.getElementById(blockId));
+    await asyncType(outputText);
+  }, [queryState]);
+
+  return (
+    <div>
+      <NodeSelect
+        items={[NODES.OR, NODES.AND, NODES.BETWEEN]}
+        onItemSelect={(item) =>
+          setQueryState({
+            type: item,
+            children: queryState.children,
+          })
+        }
+        itemRenderer={(item) => <span>{item}</span>}
+      />
+      <Button text="Save" onClick={onSave} />
+    </div>
+  );
+};
+
+const QueryBuilder = ({ blockId }: { blockId: string }) => {
   return (
     <Popover
-      content={<div>Query Builder Coming Soon!</div>}
+      content={<QueryContent blockId={blockId} />}
       target={<Button text="QUERY" />}
     />
   );
 };
 
-export default <QueryBuilder />;
+export default (blockId: string) => <QueryBuilder blockId={blockId} />;
