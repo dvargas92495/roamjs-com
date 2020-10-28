c446ebf18d46bd03d770d67218b1de94bc4893ed
David Vargas <dvargas92495@gmail.com>
2020-10-28 11:28:51 -0400

    Migrate existing docs

--- components/ExtensionLayout.tsx
+++ components/ExtensionLayout.tsx
@@ -1,22 +1,29 @@
 import { VerticalNavigationTabs } from "@dvargas92495/ui";
-import React from "react";
+import React, { useMemo } from "react";
 import Layout from "./Layout";
 import GithubSponsor from "./GithubSponsor";
 
-const ExtensionLayout = ({ children }: { children: React.ReactNode }) => (
-  <Layout>
-    <VerticalNavigationTabs
-      items={[
-        { label: "Getting Started", href: "extensions" },
-        { label: "Attr tables", href: "extensions/attr-table" },
-        { label: "Emoji", href: "extensions/emoji" },
-        { label: "Github", href: "extensions/github" },
-      ]}
-    >
-      {children}
-      <GithubSponsor />
-    </VerticalNavigationTabs>
-  </Layout>
-);
+const ExtensionLayout = ({ children }: { children: React.ReactNode }) => {
+  /*const items = useMemo(() => {
+    const fileNames = fs.readdirSync("docs/extensions");
+    return fileNames.map((f) => f.replace(/\.md$/, "")).map(f => ({
+      label: `${f.substring(0,1)}${f.replace(/-/g," ").substring(1)}`,
+      href: f,
+    }));
+  }, []);*/
+  return (
+    <Layout>
+      <VerticalNavigationTabs
+        items={[
+          { label: "Getting Started", href: "extensions" },
+         // ...items,
+        ]}
+      >
+        {children}
+        <GithubSponsor />
+      </VerticalNavigationTabs>
+    </Layout>
+  );
+};
 
 export default ExtensionLayout;
