ad0f27e8a8d06c35e35ae3c5393cffb27e6a1c4e
David Vargas <dvargas92495@gmail.com>
2020-11-02 17:03:02 -0500

    aws-email v1.1.4

--- aws.tf
+++ aws.tf
@@ -92,7 +92,7 @@ module "aws_cron_job" {
 
 module "aws_email" {
   source  = "dvargas92495/email/aws"
-  version = "1.1.3"
+  version = "1.1.4"
 
   domain = "roamjs.com"
   zone_id = module.aws_static_site.route53_zone_id
