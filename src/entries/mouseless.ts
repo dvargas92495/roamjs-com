ad6676a8ed5170aebc8b74f1c832b6266d8a7191
David Vargas <dvargas92495@gmail.com>
2020-11-03 16:31:39 -0500

    Filling out Commands (#274)
    * Filling out Commands
    
    * More commands
    
    * Demo
    
    * Demo
    
    * Refocus
    
    * Finalize

--- src/entries/mouseless.ts
+++ src/entries/mouseless.ts
@@ -1,6 +1,47 @@
-import { renderMouselessModal } from "../components/MouselessModal";
+import { renderMouselessDialog } from "../components/MouselessDialog";
+import { createHTMLObserver, isControl } from "../entry-helpers";
 
 const container = document.createElement("div");
 container.id = "roamjs-mouseless-root";
 document.body.appendChild(container);
-renderMouselessModal(container as HTMLDivElement);
+renderMouselessDialog(container as HTMLDivElement);
+
+createHTMLObserver(
+  (b) => {
+    if (b.tabIndex < 0) {
+      b.tabIndex = 0;
+    }
+  },
+  "SPAN",
+  "bp3-button"
+);
+
+document.addEventListener("keydown", (e) => {
+  if (isControl(e)) {
+    if (e.shiftKey) {
+      if (e.key === "S") {
+        const previousElement = document.activeElement as HTMLElement;
+        const emptyShortcuts = document.getElementsByClassName(
+          "bp3-button bp3-icon-star-empty"
+        ) as HTMLCollectionOf<HTMLSpanElement>;
+        const shortcuts = document.getElementsByClassName(
+          "bp3-button bp3-icon-star"
+        ) as HTMLCollectionOf<HTMLSpanElement>;
+        if (emptyShortcuts.length) {
+          emptyShortcuts[0].click();
+          previousElement?.focus();
+        } else if (shortcuts.length) {
+          shortcuts[0]?.click();
+          previousElement?.focus();
+        }
+      }
+    }
+  } else {
+    if (e.key === "Enter") {
+      const element = e.target as HTMLElement;
+      if (element.className.indexOf("bp3-button") > -1) {
+        element.click();
+      }
+    }
+  }
+});
