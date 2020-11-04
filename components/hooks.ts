b8cde1529483c658a56baaa96993e8c94e4f5856
David Vargas <dvargas92495@gmail.com>
2020-11-04 11:39:56 -0500

    Fix quote bug on copy code (#276)
    * Fix quote bug on copy code
    
    * Just quotes
    
    * Need a semicolon

--- components/hooks.ts
+++ components/hooks.ts
@@ -2,27 +2,30 @@ import { useCallback } from "react";
 
 const copyCode = (items: string[]) => (e) => {
   const scripts = items
-    .map((f) => `addScript("${f.replace(/ /g, "-").toLowerCase()}");`)
+    .map((f) => `addScript(\"${f.replace(/ /g, "-").toLowerCase()}\");`)
     .join("");
-  const codeContent = `\`\`\`const addScript = name => {
+  const codeContent = `\`\`\`var addScript = name => {
         var old = document.getElementById(name);
         if (old) {
           return;
         }  
-        var s = document.createElement('script');      
+        var s = document.createElement(\"script\");      
         s.type = \"text/javascript\";
         s.src = \`https://roamjs.com/\$\{name\}.js\`;
         s.async = true;
         s.id = name;
-        document.getElementsByTagName('head')[0].appendChild(s);
-      }
+        document.getElementsByTagName(\"head\")[0].appendChild(s);
+      };
       
       ${scripts}
       \`\`\``;
   e.clipboardData.setData(
     "text/plain",
     `- {{[[roam/js]]}}
-    - ${codeContent.replace(/\n/g, "").replace(/  /g, "")}
+    - ${codeContent
+      .replace(/\n/g, "")
+      .replace(/  /g, "")
+      .replace(/\"/g, "\u0022")}
   `
   );
   e.preventDefault();
