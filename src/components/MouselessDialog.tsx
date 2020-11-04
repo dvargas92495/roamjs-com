eae3442c8e8e3f08a9bb271eb9c4ec9f0ea8e3f6
David Vargas <dvargas92495@gmail.com>
2020-11-04 15:49:22 -0500

    Test last batch

--- src/components/MouselessDialog.tsx
+++ src/components/MouselessDialog.tsx
@@ -14,12 +14,30 @@ const os = (apple: string, windows: string) => (isApple ? apple : windows);
 const control = (key: string) => os(`Cmd-${key}`, `Ctrl-${key}`);
 
 const COMMANDS = [
+  { command: "Search Page", shortcut: control("f") },
+  { command: "Indent Block", shortcut: "Tab" },
+  { command: "Unindent Block", shortcut: "Shift-Tab" },
+  { command: "Move Block Up", shortcut: os("Ctrl-Shift-Up", "Alt-Shift-Up") },
+  {
+    command: "Move Block Down",
+    shortcut: os("Ctrl-Shift-Down", "Alt-Shift-Down"),
+  },
+  { command: "Create a New Line", shortcut: "Shift-Enter" },
+  { command: "Redo", shortcut: control("y") },
+  { command: "Add Version", shortcut: "Ctrl-Comma" },
+  { command: "Expand all Versions", shortcut: "Ctrl-Period" },
+  { command: "Cycle Version Right", shortcut: "Ctrl-Shift->" },
+  { command: "Cycle Version Left", shortcut: "Ctrl-Shift-<" },
+  { command: "Toggle Right Sidebar", shortcut: control("Shift-\\") },
+  { command: "Toggle Left Sidebar", shortcut: control("\\") },
+  { command: "Select Block Above", shortcut: "Shift-Up" },
+  { command: "Select Block Below", shortcut: "Shift-Down" },
   { command: "Close This Dialog", shortcut: "Esc" },
   { command: "Search Database", shortcut: control("u") },
-  { command: "Search Page", shortcut: control("f"), disabled: true },
   {
     command: "Open Page In Sidebar From Search Results",
     shortcut: "Shift-Enter",
+    disabled: true,
   },
   {
     command: "Jump to Daily Notes Page",
@@ -29,17 +47,11 @@ const COMMANDS = [
   { command: "Open Page In Sidebar From Block", shortcut: control("Shift-O") },
   { command: "Toggle Brackets", shortcut: control("c-b"), disabled: true },
   { command: "Toggle Namespace", shortcut: control("c-l"), disabled: true },
-  { command: "Indent Block", shortcut: "Tab" },
-  { command: "Unindent Block", shortcut: "Shift-Tab" },
-  { command: "Move Block Up", shortcut: control("Shift-Up") },
-  { command: "Move Block Down", shortcut: control("Shift-Down") },
   { command: "Create a Block", shortcut: "Enter" },
-  { command: "Create a New Line", shortcut: "Shift-Enter" },
   { command: "Undo", shortcut: control("z") },
-  { command: "Redo", shortcut: control("Shift-Z") },
   { command: "Zoom Into Block", shortcut: os("Cmd-Period", "Alt-Right") },
   { command: "Zoom Out to Parent", shortcut: os("Cmd-Comma", "Alt-Left") },
-  { command: "Open This Dialog", shortcut: "Ctrl-Shift-?" },
+  { command: "Open This Dialog", shortcut: "Ctrl-Shift-?", disabled: true },
   { command: "Collapse All Child Blocks", shortcut: control("Up") },
   { command: "Expand All Child Blocks", shortcut: control("Down") },
   { command: "Select All Blocks", shortcut: control("Shift-A") },
@@ -51,19 +63,10 @@ const COMMANDS = [
   { command: "Bold Text", shortcut: control("b") },
   { command: "Italicize", shortcut: control("i") },
   { command: "Create External Link", shortcut: control("k") },
-  { command: "Strikethrough text", shortcut: control("y") },
   { command: "Highlight Text", shortcut: control("h") },
   { command: "Open Search Dialog", shortcut: control("Shift-9") },
-  { command: "Add Version", shortcut: "Ctrl-Comma" },
-  { command: "Expand all Versions", shortcut: "Ctrl-Period" },
-  { command: "Cycle Version Right", shortcut: "Ctrl-Shift->" },
-  { command: "Cycle Version Left", shortcut: "Ctrl-Shift-<" },
-  { command: "Jump to first block", shortcut: control("Enter") },
-  { command: "Jump to last block", shortcut: control("Shift-Enter") },
-  { command: "Toggle Right Sidebar", shortcut: control("Shift-\\") },
-  { command: "Toggle Left Sidebar", shortcut: control("\\") },
-  { command: "Select Block Above", shortcut: "Shift-Up" },
-  { command: "Select Block Below", shortcut: "Shift-Down" },
+  { command: "Jump to First Block", shortcut: control("Enter") },
+  { command: "Jump to Last Block", shortcut: control("Shift-Enter") },
   { command: "Add Shortcut To Page", shortcut: control("Shift-S") },
 ];
 
@@ -81,6 +84,10 @@ const convertKey = (k: string) => {
       return "ArrowLeft";
     case "Right":
       return "ArrowRight";
+    case "Comma":
+      return ",";
+    case "Period":
+      return ".";
     default:
       return k;
   }
@@ -90,8 +97,6 @@ const convertCode = (k: string) => {
   switch (k) {
     case "Esc":
       return "Escape";
-    case "Space":
-      return "Space";
     case "Up":
       return "ArrowUp";
     case "Down":
@@ -101,7 +106,17 @@ const convertCode = (k: string) => {
     case "Right":
       return "ArrowRight";
     case "Enter":
-      return "Enter";
+    case "Tab":
+    case "Space":
+    case "Comma":
+    case "Period":
+      return k;
+    case ">":
+      return "Period";
+    case "<":
+      return "Comma";
+    case "\\":
+      return "Backslash";
     default:
       return `Key${k.toUpperCase()}`;
   }
@@ -123,6 +138,16 @@ const convertKeyCode = (k: string) => {
       return 39;
     case "Enter":
       return 13;
+    case "Tab":
+      return 9;
+    case "<":
+    case "Comma":
+      return 188;
+    case ">":
+    case "Period":
+      return 190;
+    case "\\":
+      return 220;
     default:
       return k.toUpperCase().charCodeAt(0);
   }
@@ -142,6 +167,10 @@ const convertShortcut = (shortcut: string): KeyboardEvent => {
     composed: true,
     // @ts-ignore
     keyCode: convertKeyCode(parts[parts.length - 1]),
+    returnValue:
+      parts.length == 2 &&
+      parts[0] === "Shift" &&
+      ["Up", "Down", "Enter"].indexOf(parts[1]) > -1,
   });
 };
 
@@ -186,11 +215,11 @@ const MouselessDialog = () => {
         return;
       }
       onClose();
+      if (shortcut === "Esc") {
+        return;
+      }
       const evt = convertShortcut(shortcut);
-      console.log(document.activeElement);
-      console.log(evt);
-      const cancelled = document.activeElement.dispatchEvent(evt);
-      console.log(cancelled);
+      document.activeElement.dispatchEvent(evt);
     },
     results,
   });
