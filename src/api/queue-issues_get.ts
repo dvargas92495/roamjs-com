5a4a8f3d463838493ece6b4b83c0a79d018b9e8f
David Vargas <dvargas92495@gmail.com>
2020-11-04 20:44:12 -0500

    wrap axios

--- src/api/queue-issues_get.ts
+++ src/api/queue-issues_get.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { getGithubOpts } from "../lambda-helpers";
+import { getGithubOpts, wrapAxios } from "../lambda-helpers";
 
 export const handler = () => {
   const opts = getGithubOpts();
-  return axios
+  return wrapAxios(axios
     .get(
       "https://api.github.com/repos/dvargas92495/roam-js-extensions/issues?labels=enhancement",
       opts
@@ -14,5 +14,5 @@ export const handler = () => {
         description: issue.body,
         total: 0,
       }))
-    );
+    ));
 };
