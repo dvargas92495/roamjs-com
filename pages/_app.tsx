868912c34ea8e22575a58e23b0ce435d4feb8606
David Vargas <dvargas92495@gmail.com>
2020-11-12 13:41:41 -0500

    I can personally install directly from Roam (Closes #231)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import { AppProps } from "next/app";
 import { Body, H2, ThemeProvider, H1, H3, H4, H5, H6 } from "@dvargas92495/ui";
 import React from "react";
+import { UserProvider } from "react-manage-users";
 import { MDXProvider } from "@mdx-js/react";
 import { Prism } from "react-syntax-highlighter";
 import "normalize.css/normalize.css";
-import "@blueprintjs/icons/lib/css/blueprint-icons.css"; 
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 
 const Pre: React.FunctionComponent<HTMLPreElement> = ({ children }) => (
@@ -17,34 +18,50 @@ const Code: React.FunctionComponent<HTMLElement> = ({
 }) => {
   return (
     <Prism language={(className || "").substring("language-".length)}>
-      {typeof children === 'string' ? children.trim() : children}
+      {typeof children === "string" ? children.trim() : children}
     </Prism>
   );
 };
 
-const MdxImage = (props) => (
-  <img {...props} style={{ maxWidth: 480 }} />
-);
+const MdxImage = (props) => <img {...props} style={{ maxWidth: 480 }} />;
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider>
-    <MDXProvider
-      components={{
-        h1: H1,
-        h2: H2,
-        h3: H3,
-        h4: H4,
-        h5: H5,
-        h6: H6,
-        p: Body,
-        code: Code,
-        pre: Pre,
-        img: MdxImage,
-      }}
-    >
-      <Component {...pageProps} />
-    </MDXProvider>
-  </ThemeProvider>
+  <UserProvider
+    autoLoginConfig={[
+      {
+        getToken: () =>
+          localStorage.getItem("roamToken") ||
+          process.env.NEXT_PUBLIC_ROAM_TOKEN ||
+          "",
+        getUser: () =>
+          Promise.resolve({
+            name: "David Vargas",
+            email: "dvargas92495@gmail.com",
+            avatarUrl: "unused.png",
+          }),
+      },
+    ]}
+    handleError={console.error}
+  >
+    <ThemeProvider>
+      <MDXProvider
+        components={{
+          h1: H1,
+          h2: H2,
+          h3: H3,
+          h4: H4,
+          h5: H5,
+          h6: H6,
+          p: Body,
+          code: Code,
+          pre: Pre,
+          img: MdxImage,
+        }}
+      >
+        <Component {...pageProps} />
+      </MDXProvider>
+    </ThemeProvider>
+  </UserProvider>
 );
 
 export default MyApp;
