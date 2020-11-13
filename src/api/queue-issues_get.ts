972a169538229e7eb9e4ff85cef277269e6bcedb
David Vargas <dvargas92495@gmail.com>
2020-11-13 16:07:10 -0500

    Push new queue lambdas

--- src/api/queue-issues_get.ts
+++ src/api/queue-issues_get.ts
@@ -1,25 +1,51 @@
 import axios from "axios";
-import { getGithubOpts, headers, wrapAxios } from "../lambda-helpers";
+import {
+  Contracts,
+  getFlossActiveContracts,
+  getGithubOpts,
+  headers,
+} from "../lambda-helpers";
 
-export const handler = () => {
+export const handler = async () => {
   const opts = getGithubOpts();
-  return axios
+  const flossIssues = await getFlossActiveContracts()
+    .then((r) =>
+      r.issues.filter(({ link }) =>
+        link.startsWith(
+          "https://github.com/dvargas92495/roam-js-extensions/issues"
+        )
+      )
+    )
+    .catch(() => [] as Contracts);
+  const issuesWithContract = new Set(flossIssues.map((i) => i.link));
+  const githubIssuesByLink = await axios
     .get(
       "https://api.github.com/repos/dvargas92495/roam-js-extensions/issues?labels=enhancement",
       opts
     )
-    .then((r) => ({
-      statusCode: 200,
-      body: JSON.stringify(r.data.map((issue: { title: string, html_url: string }) => ({
-        name: issue.title,
-        htmlUrl: issue.html_url,
-        total: 0,
-      }))),
-      headers,
-    }))
-    .catch((e) => ({
-      statusCode: e.response?.status || 500,
-      body: e.response?.data ? JSON.stringify(e.response.data) : e.message,
-      headers,
-    }));
+    .then((r) =>
+      Object.fromEntries(
+        r.data.map((issue: { title: string; html_url: string }) => [
+          issue.html_url,
+          {
+            name: issue.title,
+            htmlUtl: issue.html_url,
+          },
+        ])
+      )
+    );
+  const body = [
+    ...flossIssues.map((i) => ({
+      total: i.reward,
+      ...githubIssuesByLink[i.link],
+    })),
+    ...Object.keys(githubIssuesByLink)
+      .filter((l) => !issuesWithContract.has(l))
+      .map((i) => ({ total: 0, ...githubIssuesByLink[i] })),
+  ];
+  return {
+    statusCode: 200,
+    body: JSON.stringify(body),
+    headers,
+  };
 };
--- src/api/queue-projects_get.ts
+++ src/api/queue-projects_get.ts
@@ -1,31 +1,57 @@
 import axios from "axios";
-import { getGithubOpts, headers } from "../lambda-helpers";
+import {
+  Contracts,
+  getFlossActiveContracts,
+  getGithubOpts,
+  headers,
+} from "../lambda-helpers";
 
-export const handler = () => {
+export const handler = async () => {
   const opts = getGithubOpts();
-  return axios
+
+  const flossProjects = await getFlossActiveContracts()
+    .then((r) =>
+      r.projects.filter(({ link }) =>
+        link.startsWith(
+          "https://github.com/dvargas92495/roam-js-extensions/projects"
+        )
+      )
+    )
+    .catch(() => [] as Contracts);
+  const projectsWithContract = new Set(flossProjects.map((p) => p.link));
+  const githubProjectsByLink = await axios
     .get(
       "https://api.github.com/repos/dvargas92495/roam-js-extensions/projects",
       opts
     )
-    .then((r) => ({
-      statusCode: 200,
-      body: JSON.stringify(
+    .then((r) =>
+      Object.fromEntries(
         r.data
           .filter(
             (project: { name: string }) => project.name !== "Site Improvements"
           )
-          .map((project: { name: string; html_url: string }) => ({
-            name: project.name,
-            htmlUrl: project.html_url,
-            total: 0,
-          }))
-      ),
-      headers,
-    }))
-    .catch((e) => ({
-      statusCode: e.response?.status || 500,
-      body: e.response?.data ? JSON.stringify(e.response.data) : e.message,
-      headers,
-    }));
+          .map((issue: { name: string; html_url: string }) => [
+            issue.html_url,
+            {
+              name: issue.name,
+              htmlUtl: issue.html_url,
+            },
+          ])
+      )
+    );
+  const body = [
+    ...flossProjects.map((i) => ({
+      total: i.reward,
+      ...githubProjectsByLink[i.link],
+    })),
+    ...Object.keys(githubProjectsByLink)
+      .filter((l) => !projectsWithContract.has(l))
+      .map((i) => ({ total: 0, ...githubProjectsByLink[i] })),
+  ];
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(body),
+    headers,
+  };
 };
