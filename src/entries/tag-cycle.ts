f72dc708fdd3bb6fc322945d038b43ed26261e75
David Vargas <dvargas92495@gmail.com>
2020-11-19 15:44:58 -0500

    Debugging

--- src/entries/tag-cycle.ts
+++ src/entries/tag-cycle.ts
@@ -23,32 +23,31 @@ getTextTreeByPageName("roam/js/tag-cycle")
             const oldvalue = textarea.value;
             const cycleTags = async (i: number) => {
               const tag1 = cycle[i];
-              if (textarea.value.includes(tag1)) {
-                const start = textarea.selectionStart;
-                const end = textarea.selectionEnd;
-                const tag2 = cycle[(i + 1 + cycle.length) % cycle.length];
-                await replaceTagText([tag1, tag2]);
-                textarea.setSelectionRange(
-                  start - tag1.length + tag2.length,
-                  end - tag1.length + tag2.length
-                );
-                e.preventDefault();
-                e.stopPropagation();
-                return true;
-              }
-              return false;
+              const start = textarea.selectionStart;
+              const end = textarea.selectionEnd;
+              const tag2 = cycle[(i + 1 + cycle.length) % cycle.length];
+              await replaceTagText([tag1, tag2]);
+              textarea.setSelectionRange(
+                start - tag1.length + tag2.length,
+                end - tag1.length + tag2.length
+              );
+              e.preventDefault();
+              e.stopPropagation();
             };
             for (let i = 0; i < cycle.length; i++) {
-              if (!cycle[i]) {
-                continue;
-              }
-              if (cycleTags(i)) {
-                  break;
+              const tag1 = cycle[i];
+              if (
+                textarea.value.includes(`#[[${tag1}]]`) ||
+                textarea.value.includes(`[[${tag1}]]`) ||
+                textarea.value.includes(`#${tag1}`)
+              ) {
+                cycleTags(i);
+                break;
               }
             }
             if (oldvalue === textarea.value) {
-                const emptyIndex = cycle.indexOf("");
-                cycleTags(emptyIndex);
+              const emptyIndex = cycle.indexOf("");
+              cycleTags(emptyIndex);
             }
           }
         }
