47c0ca902fe015e6a690d8c364f91c0d4189e54a
David Vargas <dvargas92495@gmail.com>
2020-11-25 14:12:28 -0500

    Dynamically fetch an individual queue page

--- pages/queue/[issueTitle]/[number].tsx
+++ pages/queue/[issueTitle]/[number].tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Markdown from "markdown-to-jsx";
 import {
   API_URL,
@@ -8,54 +8,83 @@ import {
   QueueItemResponse,
 } from "../../../components/constants";
 import StandardLayout from "../../../components/StandardLayout";
-import { ExternalLink, H2, H4, H5, Items, Subtitle } from "@dvargas92495/ui";
+import {
+  DataLoader,
+  ExternalLink,
+  H2,
+  H4,
+  H5,
+  Items,
+  Subtitle,
+} from "@dvargas92495/ui";
 import FundButton from "../../../components/FundButton";
 
 const QueueItemPage = ({
-  link,
-  title,
-  body,
-  contracts,
+  number,
   issueTitle,
 }: {
-  issueTitle: "extension" | "enhancement",
-  link: string;
-  title: string;
-  body: string;
-  contracts: {
-    dueDate: string;
-    reward: number;
-    createdDate: string;
-    createdBy: string;
-    uuid: string;
-  }[];
-}) => (
-  <StandardLayout>
-    <H2>{title}</H2>
-    <Subtitle>
-      <ExternalLink href={link}>GitHub Link</ExternalLink>
-    </Subtitle>
-    <H4>Description</H4>
-    <div style={{marginBottom: 16}}>
-      <Markdown>{body}</Markdown>
-    </div>
-    <H4>Backers</H4>
-    {contracts.length === 0 ? (
-      <Subtitle>
-        No one has funded this issue. Fund it to move it up the priority queue!
-      </Subtitle>
-    ) : (
-      <Items
-        items={contracts.map((c) => ({
-          primary: c.createdBy,
-          secondary: `Funded $${c.reward} on ${c.createdDate}, expiring on ${c.dueDate}.`,
-          key: c.uuid,
-        }))}
-      />
-    )}
-    <FundButton title={issueTitle} name={title} url={link} />
-  </StandardLayout>
-);
+  issueTitle: "extension" | "enhancement";
+  number: string;
+}) => {
+  const [issue, setIssue] = useState<{
+    link: string;
+    title: string;
+    body: string;
+    contracts: {
+      dueDate: string;
+      reward: number;
+      createdDate: string;
+      createdBy: string;
+      uuid: string;
+    }[];
+  }>({
+    link: "",
+    title: "",
+    body: "",
+    contracts: [],
+  });
+  const loadAsync = useCallback(
+    () =>
+      axios
+        .get(
+          `${FLOSS_API_URL}/issue?link=dvargas92495/roam-js-extensions/issues/${number}`
+        )
+        .then((r) => setIssue(r.data)),
+    [setIssue]
+  );
+  return (
+    <StandardLayout>
+      <DataLoader loadAsync={loadAsync}>
+        <H2>{issue.title}</H2>
+        <Subtitle>
+          <ExternalLink href={issue.link}>GitHub Link</ExternalLink>
+        </Subtitle>
+        <H4>Description</H4>
+        <div style={{ marginBottom: 16 }}>
+          <Markdown>{issue.body}</Markdown>
+        </div>
+        <H4>Backers</H4>
+        {issue.contracts.length === 0 ? (
+          <div style={{ marginBottom: 16 }}>
+            <Subtitle>
+              No one has funded this issue. Fund it to move it up the priority
+              queue!
+            </Subtitle>
+          </div>
+        ) : (
+          <Items
+            items={issue.contracts.map((c) => ({
+              primary: c.createdBy,
+              secondary: `Funded $${c.reward} on ${c.createdDate}, expiring on ${c.dueDate}.`,
+              key: c.uuid,
+            }))}
+          />
+        )}
+        <FundButton title={issueTitle} name={issue.title} url={issue.link} />
+      </DataLoader>
+    </StandardLayout>
+  );
+};
 
 const mapIssues = ({
   res,
@@ -86,14 +115,12 @@ export const getStaticPaths: GetStaticPaths = async () =>
   }));
 
 export const getStaticProps: GetStaticProps = async (
-  context: GetStaticPropsContext<{ number: string, issueTitle: "extension" | "enhancement" }>
-) =>
-  axios
-    .get(
-      `${FLOSS_API_URL}/issue?link=dvargas92495/roam-js-extensions/issues/${context.params.number}`
-    )
-    .then((r) => ({
-      props: {...r.data, issueTitle: context.params.issueTitle}
-    }));
+  context: GetStaticPropsContext<{
+    number: string;
+    issueTitle: "extension" | "enhancement";
+  }>
+) => ({
+  props: context.params,
+});
 
 export default QueueItemPage;
