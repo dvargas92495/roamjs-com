fefcafc4cb35620f6d5326d2572d762fa2e5a576
David Vargas <dvargas92495@gmail.com>
2020-12-02 15:40:34 -0800

    Fix mixpanel track

--- src/api/article_post.ts
+++ src/api/article_post.ts
@@ -1,9 +1,9 @@
 import { APIGatewayEvent } from "aws-lambda";
 import axios from "axios";
-import mixpanel from "mixpanel";
+import Mixpanel from "mixpanel";
 import { wrapAxios } from "../lambda-helpers";
 
-mixpanel.init(process.env.MIXPANEL_TOKEN);
+const mixpanel = Mixpanel.init(process.env.MIXPANEL_TOKEN);
 
 export const handler = async (event: APIGatewayEvent) => {
   mixpanel.track("Use Extension", {
