e3853d1d6533344f5d0e8a4bb7056fd3f5376625
David Vargas <dvargas92495@gmail.com>
2020-12-03 16:20:33 -0800

    Webpack

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -4,8 +4,8 @@ module.exports = (env) => ({
     ...common(env),
     mode: 'development',
     performance: {
-      hints: "error",
-      maxEntrypointSize: 5588910,
-      maxAssetSize: 5588910,
+      hints: "error",    
+      maxEntrypointSize: 13946000,
+      maxAssetSize: 13946000,
     },
 });
