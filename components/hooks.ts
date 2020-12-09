136da27eb9e620f6c290ac2573ed3c2865a91e72
David Vargas <dvargas92495@gmail.com>
2020-12-09 10:01:29 -0800

    Finally fix Copy Code

--- components/hooks.ts
+++ components/hooks.ts
@@ -4,30 +4,37 @@ import { isMobile } from "react-device-detect";
 const copyCode = (items: string[]) => (e) => {
   const scripts = items
     .map((f) => `installScript(\"${f.replace(/ /g, "-").toLowerCase()}\");`)
-    .join("");
-  const codeContent = `\`\`\`var installScript = name => {
-        var existing = document.getElementById(name);
-        if (existing) {
-          return;
-        }  
-        var extension = document.createElement(\"script\");      
-        extension.type = \"text/javascript\";
-        extension.src = \`https://roamjs.com/\$\{name\}.js\`;
-        extension.async = true;
-        extension.id = name;
-        document.getElementsByTagName(\"head\")[0].appendChild(extension);
-      };
-      
-      ${scripts}
-      \`\`\``;
+    .join("\n");
+  const codeContent = `\`\`\`javascript
+var installScript = name => {
+  var existing = document.getElementById(name);
+  if (existing) {
+    return;
+  }  
+  var extension = document.createElement(\"script\");      
+  extension.type = \"text/javascript\";
+  extension.src = \`https://roamjs.com/\$\{name\}.js\`;
+  extension.async = true;
+  extension.id = name;
+  document.getElementsByTagName(\"head\")[0].appendChild(extension);
+};
+${scripts}
+    \`\`\``;
   e.clipboardData.setData(
     "text/plain",
-    `- {{[[roam/js]]}}
-    - ${codeContent
-      .replace(/\n/g, "")
-      .replace(/  /g, "")
+    `{{[[roam/js]]}}
+    ${codeContent
       .replace(/\"/g, "\u0022")}
   `
+  );
+  e.clipboardData.setData(
+    "roam/data",
+    `["^ ","~:type","~:copy","~:copied-data",[["^ ","~:block/string","{{[[roam/js]]}}","~:block/children",[["^ ","~:block/string","~${codeContent
+      .replace(/\n/g, "\\n")
+      .replace(
+        /\"/g,
+        "\\\""
+      )}","~:block/open",true,"~:block/order",0]],"~:block/open",true]]]`
   );
   e.preventDefault();
 };
