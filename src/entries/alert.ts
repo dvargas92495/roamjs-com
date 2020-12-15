add7593f6203ca5862a3477cf154b4d48e3dcd83
David Vargas <dvargas92495@gmail.com>
2020-12-15 08:59:16 -0800

    Entry demo log

--- src/entries/alert.ts
+++ src/entries/alert.ts
@@ -2,6 +2,7 @@ import { createButtonObserver, runExtension } from "../entry-helpers";
 import { render } from '../components/AlertButton';
 
 runExtension("alert", () => {
+  console.log('running alert');  
   createButtonObserver({
     shortcut: "alert",
     attribute: "alert-button",
