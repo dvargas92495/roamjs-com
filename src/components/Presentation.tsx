e7f71d600904d9ae8d4a01ff6090ba6bb542cf10
David Vargas <dvargas92495@gmail.com>
2020-12-27 18:06:43 -0500

    loaded and display block

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -97,27 +97,32 @@ const ContentSlide = ({
   note: Slide;
 }) => {
   const contentRef = useRef<HTMLDivElement>(null);
+  const [loaded, setLoaded] = useState(false);
   const [transform, setTransform] = useState("initial");
   useEffect(() => {
-    contentRef.current.style.display = 'block';
-    const containerHeight = contentRef.current?.offsetHeight;
-    if (containerHeight > 0) {
-      const setScale = () => {
-        const contentHeight = (contentRef.current.children[0] as HTMLElement)
-          .offsetHeight;
-        if (contentHeight > containerHeight) {
-          const scale = containerHeight / contentHeight;
-          setTransform(`scale(${scale})`);
-        } else {
-          setTransform("initial");
-        }
-      };
-      setScale();
-      Array.from(contentRef.current.getElementsByTagName("img")).forEach(
-        (i) => (i.onload = setScale)
-      );
+    if (loaded) {
+      const containerHeight = contentRef.current?.offsetHeight;
+      if (containerHeight > 0) {
+        const setScale = () => {
+          const contentHeight = (contentRef.current.children[0] as HTMLElement)
+            .offsetHeight;
+          if (contentHeight > containerHeight) {
+            const scale = containerHeight / contentHeight;
+            setTransform(`scale(${scale})`);
+          } else {
+            setTransform("initial");
+          }
+        };
+        setScale();
+        Array.from(contentRef.current.getElementsByTagName("img")).forEach(
+          (i) => (i.onload = setScale)
+        );
+      }
+    } else {
+      setLoaded(true);
+      contentRef.current.style.display = "block";
     }
-  }, [contentRef.current, setTransform]);
+  }, [contentRef.current, setTransform, loaded, setLoaded]);
   return (
     <section style={{ textAlign: "left" }}>
       <h1>{text}</h1>
