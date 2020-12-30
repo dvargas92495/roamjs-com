dbab3c30c7f382fb5af2a0789b72c8f941184c37
David Vargas <dvargas92495@gmail.com>
2020-12-29 19:18:05 -0500

    Bump subscribers

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,7 +53,7 @@ const HomePage = (): JSX.Element => (
           label: "Daily Downloads",
         },
         {
-          value: "120+",
+          value: "130+",
           label: "Subscribers",
         },
       ]}
