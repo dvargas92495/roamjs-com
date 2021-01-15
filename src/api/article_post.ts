e8be49af4be939abff87ff482f46860419bc7d11
David Vargas <dvargas92495@gmail.com>
2021-01-14 22:10:38 -0500

    Article fetch retries without useragent

--- src/api/article_post.ts
+++ src/api/article_post.ts
@@ -1,5 +1,5 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import Mixpanel from "mixpanel";
 import { headers } from "../lambda-helpers";
 import charset from "charset";
@@ -14,28 +14,36 @@ export const handler = async (
     action: "Import",
   });
   const { url } = JSON.parse(event.body);
-  return axios
-    .get(url, {
-      headers: { "Content-type": "text/html", "user-agent": "Mozilla/5.0" },
-      responseType: "document",
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore https://github.com/axios/axios/pull/2619
-      responseEncoding: "base64",
-    })
-    .then((r) => {
-      const enc = charset(r.headers) || "utf8";
-      return {
-        statusCode: 200,
-        body: JSON.stringify(r.data),
-        headers: {
-          ...headers,
-          "Content-Type": `application/json;charset=${enc}`,
-        },
-      };
-    })
-    .catch((e) => ({
-      statusCode: e.response?.status || 500,
-      body: e.response?.data ? JSON.stringify(e.response.data) : e.message,
-      headers,
-    }));
+  const fetch = (requestHeaders: { [key: string]: string }) =>
+    axios
+      .get(url, {
+        headers: requestHeaders,
+        responseType: "document",
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore https://github.com/axios/axios/pull/2619
+        responseEncoding: "base64",
+      })
+      .then((r) => {
+        const enc = charset(r.headers) || "utf8";
+        return {
+          statusCode: 200,
+          body: JSON.stringify(r.data),
+          headers: {
+            ...headers,
+            "Content-Type": `application/json;charset=${enc}`,
+          },
+        };
+      });
+  const handleError = (e: Error & AxiosError) => ({
+    statusCode: e.response?.status || 500,
+    body: e.response?.data ? JSON.stringify(e.response.data) : e.message,
+    headers,
+  });
+  return fetch({ "Content-type": "text/html", "user-agent": "Mozilla/5.0" })
+    .catch((e) =>
+      e.response.status === 502
+        ? fetch({ "Content-type": "text/html" })
+        : handleError(e)
+    )
+    .catch(handleError);
 };
