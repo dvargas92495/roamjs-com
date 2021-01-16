8bbad10760a6f226e6aa1fc9cff99f1c4080690d
David Vargas <dvargas92495@gmail.com>
2021-01-15 22:18:06 -0500

    Drawer improvements

--- src/components/MarkmapPanel.tsx
+++ src/components/MarkmapPanel.tsx
@@ -1,25 +1,36 @@
 import { Drawer, MenuItem, Position } from "@blueprintjs/core";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
 const MarkmapPanel: React.FunctionComponent = () => {
   const [isOpen, setIsOpen] = useState(false);
   const open = useCallback(() => setIsOpen(true), [setIsOpen]);
   const close = useCallback(() => setIsOpen(false), [setIsOpen]);
+  const containerRef = useRef(null);
+  useEffect(() => {
+    if (containerRef.current) {
+      const overlay = containerRef.current.closest(
+        ".bp3-overlay-container"
+      ) as HTMLDivElement;
+      if (overlay) {
+        overlay.style.pointerEvents = "none";
+      }
+    }
+  }, [containerRef]);
   return (
     <>
       <MenuItem text={"Open Markmap"} onClick={open} />
       <Drawer
-        icon="info-sign"
         onClose={close}
         title="Markmap Panel"
         isOpen={isOpen}
-        position={Position.RIGHT}
+        position={Position.BOTTOM}
         hasBackdrop={false}
         canOutsideClickClose={false}
         canEscapeKeyClose
+        enforceFocus={false}
       >
-        Markmap!
+        <div ref={containerRef}>Markmap!</div>
       </Drawer>
     </>
   );
