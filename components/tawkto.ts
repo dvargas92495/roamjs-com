3d7005296a34675ef4a36c225e008089a583cc41
David Vargas <dvargas92495@gmail.com>
2021-01-17 22:25:58 -0500

    Tawkto integration (Closes #402)

---
+++ components/tawkto.ts
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+const loader = () => {
+  window.Tawk_API={}
+  window.Tawk_LoadStart=new Date();
+  const s1=document.createElement("script")
+  s1.async=true;
+  s1.src='https://embed.tawk.to/6004fa7bc31c9117cb6f97fd/1es9n4stl';
+  s1.setAttribute('crossorigin','*');
+  document.head.appendChild(s1);
+};
+
+declare global {
+  interface Window {
+    Tawk_API?: {[key: string]: string};
+    Tawk_LoadStart:Date;
+  }
+}
+
+const useTawkTo = (): void => {
+  useEffect(() => {
+    loader();
+  }, []);
+};
+
+export default useTawkTo;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import "normalize.css/normalize.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import { AUTH0_AUDIENCE, AUTH0_DOMAIN } from "../components/constants";
+import useTawkTo from "../components/tawkto";
 
 const Pre: React.FunctionComponent<HTMLPreElement> = ({ children }) => (
   <>{children}</>
@@ -26,34 +27,37 @@ const Code: React.FunctionComponent<HTMLElement> = ({
 
 const MdxImage = (props) => <img {...props} style={{ maxWidth: 480 }} />;
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <Auth0Provider
-    domain={AUTH0_DOMAIN}
-    clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
-    useRefreshTokens={true}
-    cacheLocation={"localstorage"}
-    audience={AUTH0_AUDIENCE}
-    scope={"read:current_user"}
-  >
-    <ThemeProvider>
-      <MDXProvider
-        components={{
-          h1: H1,
-          h2: H2,
-          h3: H3,
-          h4: H4,
-          h5: H5,
-          h6: H6,
-          p: Body,
-          code: Code,
-          pre: Pre,
-          img: MdxImage,
-        }}
-      >
-        <Component {...pageProps} />
-      </MDXProvider>
-    </ThemeProvider>
-  </Auth0Provider>
-);
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  useTawkTo();
+  return (
+    <Auth0Provider
+      domain={AUTH0_DOMAIN}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      useRefreshTokens={true}
+      cacheLocation={"localstorage"}
+      audience={AUTH0_AUDIENCE}
+      scope={"read:current_user"}
+    >
+      <ThemeProvider>
+        <MDXProvider
+          components={{
+            h1: H1,
+            h2: H2,
+            h3: H3,
+            h4: H4,
+            h5: H5,
+            h6: H6,
+            p: Body,
+            code: Code,
+            pre: Pre,
+            img: MdxImage,
+          }}
+        >
+          <Component {...pageProps} />
+        </MDXProvider>
+      </ThemeProvider>
+    </Auth0Provider>
+  );
+};
 
 export default MyApp;
