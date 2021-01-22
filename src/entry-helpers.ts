fec753b3772366eb08d8ba3df11134c053e4bbd1
David Vargas <dvargas92495@gmail.com>
2021-01-22 15:55:07 -0500

    Replace Text Selection madness

--- src/entry-helpers.ts
+++ src/entry-helpers.ts
@@ -70,7 +70,7 @@ export const runExtension = async (
   run();
 };
 
-export const replaceText = async ({
+export const replaceText = ({
   before,
   after,
   prepend,
@@ -78,18 +78,39 @@ export const replaceText = async ({
   before: string;
   after: string;
   prepend?: boolean;
-}): Promise<void> => {
+}): void => {
   const textArea = document.activeElement as HTMLTextAreaElement;
+  const oldValue = textArea.value;
+  const start = textArea.selectionStart;
+  const end = textArea.selectionEnd;
   const text = !before
     ? prepend
-      ? `${after} ${textArea.value}`
-      : `${textArea.value}${after}`
-    : textArea.value.replace(`${before}${!after && prepend ? " " : ""}`, after);
+      ? `${after} ${oldValue}`
+      : `${oldValue}${after}`
+    : oldValue.replace(`${before}${!after && prepend ? " " : ""}`, after);
   const { blockUid } = getUids(textArea);
   window.roamAlphaAPI.updateBlock({ block: { string: text, uid: blockUid } });
+  const diff = text.length - oldValue.length;
+  if (diff !== 0) {
+    let index = 0;
+    const maxIndex = Math.min(
+      Math.max(oldValue.length, text.length),
+      Math.max(start, end) + 1
+    );
+    for (index = 0; index < maxIndex; index++) {
+      if (oldValue.charAt(index) !== text.charAt(index)) {
+        break;
+      }
+    }
+    const newStart = index > start ? start : start + diff;
+    const newEnd = index > end ? end : end + diff;
+    if (newStart !== start || newEnd !== newEnd) {
+      setTimeout(() => textArea.setSelectionRange(newStart, newEnd), 100);
+    }
+  }
 };
 
-export const replaceTagText = async ({
+export const replaceTagText = ({
   before,
   after,
   addHash = false,
@@ -99,14 +120,14 @@ export const replaceTagText = async ({
   after: string;
   addHash?: boolean;
   prepend?: boolean;
-}): Promise<void> => {
+}): void => {
   if (before) {
-    await replaceText({
+    replaceText({
       before: `#[[${before}]]`,
       after: after ? `#[[${after}]]` : "",
       prepend,
     });
-    await replaceText({
+    replaceText({
       before: `[[${before}]]`,
       after: after ? `[[${after}]]` : "",
       prepend,
@@ -114,7 +135,7 @@ export const replaceTagText = async ({
     const hashAfter = after.match(/(\s|\[\[.*\]\])/)
       ? `#[[${after}]]`
       : `#${after}`;
-    await replaceText({
+    replaceText({
       before: `#${before}`,
       after: after ? hashAfter : "",
       prepend,
@@ -123,9 +144,9 @@ export const replaceTagText = async ({
     const hashAfter = after.match(/(\s|\[\[.*\]\])/)
       ? `#[[${after}]]`
       : `#${after}`;
-    await replaceText({ before: "", after: hashAfter, prepend });
+    replaceText({ before: "", after: hashAfter, prepend });
   } else {
-    await replaceText({ before: "", after: `[[${after}]]`, prepend });
+    replaceText({ before: "", after: `[[${after}]]`, prepend });
   }
 };
 
