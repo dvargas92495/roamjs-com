4e118f2aaecd3c4a0dae9dbf7d776576ee5f3f2d
David Vargas <dvargas92495@gmail.com>
2021-02-02 11:32:25 -0500

    Update Scopes

--- components/AddToSlack.tsx
+++ components/AddToSlack.tsx
@@ -40,7 +40,7 @@ const AddToSlack: React.FunctionComponent = () => {
         Slack workspace's token.
       </Body>
       <a
-        href={`https://slack.com/oauth/v2/authorize?client_id=${process.env.NEXT_PUBLIC_SLACK_CLIENT_ID}&scope=channels:read,incoming-webhook,im:write,chat:write,users:read&user_scope=`}
+        href={`https://slack.com/oauth/v2/authorize?client_id=${process.env.NEXT_PUBLIC_SLACK_CLIENT_ID}&scope=channels:read,chat:write,users:read,users:read.email&user_scope=`}
       >
         <img
           alt="Add to Slack"
