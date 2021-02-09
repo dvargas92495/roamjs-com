830f015044a2df93c90256980746fd2a65d53c23
David Vargas <dvargas92495@gmail.com>
2021-02-09 10:49:19 -0600

    TwitterAuth component in docs

---
+++ components/TwitterAuth.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+
+const TwitterAuth: React.FunctionComponent = () => {
+  useEffect(() => {
+    if (window.opener && window.opener !== window) {
+      const query = new URLSearchParams(window.location.search);
+      const isAuth = query.get("auth");
+      if (isAuth) {
+        const oauth_token = query.get("oauth_token");
+        const oauth_verifier = query.get("oauth_verifier");
+        if (oauth_token && oauth_verifier) {
+          window.postMessage(
+            JSON.stringify({ oauth_token, oauth_verifier }),
+            "https://roamresearch.com"
+          );
+        }
+      }
+    }
+  }, []);
+  return <></>;
+};
+
+export default TwitterAuth;
