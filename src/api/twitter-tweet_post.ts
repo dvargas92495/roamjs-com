4cd037a700fb8ab8b81f69bf876022c473d51bc1
David Vargas <dvargas92495@gmail.com>
2021-02-09 13:18:35 -0600

    Fix token secret handling

--- src/api/twitter-tweet_post.ts
+++ src/api/twitter-tweet_post.ts
@@ -3,22 +3,28 @@ import axios from "axios";
 import { headers, twitterOAuth } from "../lambda-helpers";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  const { oauthData, content } = JSON.parse(event.body || "{}");
+  const { key, secret, content } = JSON.parse(event.body || "{}");
   const url = `https://api.twitter.com/1.1/statuses/update.json?status=${encodeURIComponent(
     content
   )}`;
   const oauthHeaders = twitterOAuth.toHeader(
-    twitterOAuth.authorize({
-      data: oauthData,
-      url,
-      method: "POST",
-    })
+    twitterOAuth.authorize(
+      {
+        url,
+        method: "POST",
+      },
+      { key, secret }
+    )
   );
 
   return axios
-    .post(url, oauthData, {
-      headers: oauthHeaders,
-    })
+    .post(
+      url,
+      {},
+      {
+        headers: oauthHeaders,
+      }
+    )
     .then((r) => ({
       statusCode: 200,
       body: JSON.stringify(r.data),
