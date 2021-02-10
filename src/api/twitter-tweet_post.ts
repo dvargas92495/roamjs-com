d1f21190912935da87380bacc2e26ce25ab2b4d3
David Vargas <dvargas92495@gmail.com>
2021-02-10 15:51:15 -0600

    Update Tweet api lambda

--- src/api/twitter-tweet_post.ts
+++ src/api/twitter-tweet_post.ts
@@ -1,11 +1,17 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import axios from "axios";
 import { headers, twitterOAuth } from "../lambda-helpers";
+import querystring from "querystring";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  const { key, secret, content } = JSON.parse(event.body || "{}");
-  const url = `https://api.twitter.com/1.1/statuses/update.json?status=${encodeURIComponent(
-    content
+  const { key, secret, content, in_reply_to_status_id } = JSON.parse(
+    event.body || "{}"
+  );
+  const data = in_reply_to_status_id
+    ? { status: content, in_reply_to_status_id }
+    : { status: content };
+  const url = `https://api.twitter.com/1.1/statuses/update.json?${querystring.stringify(
+    data
   )}`;
   const oauthHeaders = twitterOAuth.toHeader(
     twitterOAuth.authorize(
