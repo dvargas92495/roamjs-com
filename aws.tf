d2e8df5b25de715281af6f761089dce2815812b5
David Vargas <dvargas92495@gmail.com>
2021-02-16 13:54:36 -0600

    Add Clerk Cnames

--- aws.tf
+++ aws.tf
@@ -155,6 +155,54 @@ module "aws_email" {
   } 
 }
 
+resource "aws_route53_record" "clerk-accounts" {
+  zone_id = module.aws_static_site.route53_zone_id
+  name    = "accounts"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["accounts.clerk.services"]
+}
+
+resource "aws_route53_record" "clerk-fe" {
+  zone_id = module.aws_static_site.route53_zone_id
+  name    = "clerk"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["frontend-api.clerk.services"]
+}
+
+resource "aws_route53_record" "clerk-fe-api" {
+  zone_id = module.aws_static_site.route53_zone_id
+  name    = "clerk.api"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["frontend-api.clerk.services"]
+}
+
+resource "aws_route53_record" "clerk-s1" {
+  zone_id = module.aws_static_site.route53_zone_id
+  name    = "s1._domainkey"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["dkim1.q5lvvno2col9.clerk.services"]
+}
+
+resource "aws_route53_record" "clerk-s2" {
+  zone_id = module.aws_static_site.route53_zone_id
+  name    = "s2._domainkey"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["dkim2.q5lvvno2col9.clerk.services"]
+}
+
+resource "aws_route53_record" "clerk-mail" {
+  zone_id = module.aws_static_site.route53_zone_id
+  name    = "mail"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["mail.q5lvvno2col9.clerk.services"]
+}
+
 data "aws_api_gateway_rest_api" "floss" {
   name = "floss"
 }
