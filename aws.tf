66cba6be9f5691e840c8727b73051a9fb691b4b0
David Vargas <dvargas92495@gmail.com>
2021-02-20 15:28:52 -0600

    serverless 1.4.1

--- aws.tf
+++ aws.tf
@@ -100,7 +100,7 @@ module "aws_static_site" {
 
 module "aws-serverless-backend" {
     source  = "dvargas92495/serverless-backend/aws"
-    version = "1.4.0"
+    version = "1.4.1"
 
     api_name = "roam-js-extensions"
     domain = "roamjs.com"
