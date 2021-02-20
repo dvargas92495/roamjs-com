4762ab0f19d51075713da8da8f0adaf0c90ad2db
David Vargas <dvargas92495@gmail.com>
2021-02-20 16:03:28 -0600

    Filter Postman headers

--- src/api/postman_post.ts
+++ src/api/postman_post.ts
@@ -3,19 +3,31 @@ import { headers } from "../lambda-helpers";
 
 export const handler = async (
   event: APIGatewayEvent
-): Promise<APIGatewayProxyResult> => ({
-  statusCode: 200,
-  body: JSON.stringify({
-    body: `This was the body sent: ${JSON.stringify(
-      JSON.parse(event.body),
-      null,
-      4
-    )}`,
-    headers: `These were the headers sent: ${JSON.stringify(
-      event.headers,
-      null,
-      4
-    )}`,
-  }),
-  headers,
-});
+): Promise<APIGatewayProxyResult> => {
+  const filteredHeaders = Object.fromEntries(
+    Object.keys(event.headers)
+      .filter(
+        (k) =>
+          !k.toLowerCase().includes("cloudfront") &&
+          !k.toLowerCase().includes("amz") &&
+          !event.headers[k].toLowerCase().includes("cloudfront")
+      )
+      .map((k) => [k, event.headers[k]])
+  );
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      body: `This was the body sent: ${JSON.stringify(
+        JSON.parse(event.body),
+        null,
+        4
+      )}`,
+      headers: `These were the headers sent: ${JSON.stringify(
+        event.headers,
+        null,
+        4
+      )}`,
+    }),
+    headers,
+  };
+};
