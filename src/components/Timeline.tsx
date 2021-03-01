1612bd04e38aec671973f56b298d91c993b2a357
David Vargas <dvargas92495@gmail.com>
2021-02-28 23:54:53 -0600

    Send Timeline to users

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -6,8 +6,9 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { Icon } from "@blueprintjs/core";
+import { Button, Icon, InputGroup, Label } from "@blueprintjs/core";
 import {
+  generateBlockUid,
   getTreeByBlockUid,
   getTreeByPageName,
   getUidsFromId,
@@ -19,6 +20,7 @@ import {
   DAILY_NOTE_PAGE_REGEX,
   extractTag,
   getTitlesReferencingPagesInSameBlockTree,
+  resolveRefs,
 } from "../entry-helpers";
 
 type TimelineProps = { blockId: string };
@@ -34,13 +36,20 @@ const reduceChildren = (prev: string, cur: TreeNode, l: number): string =>
     ""
   )}`;
 
+const getTag = (blockId: string) => {
+  const { blockUid } = getUidsFromId(blockId);
+  const tree = getTreeByBlockUid(blockUid);
+  const tagNode = tree.children.find((t) => /tag/i.test(t.text));
+  if (tagNode && tagNode.children.length) {
+    return extractTag(tagNode.children[0].text);
+  }
+  return "";
+};
+
 const Timeline: React.FunctionComponent<TimelineProps> = ({ blockId }) => {
   const getTimelineElements = useCallback(() => {
-    const { blockUid } = getUidsFromId(blockId);
-    const tree = getTreeByBlockUid(blockUid);
-    const tagNode = tree.children.find((t) => /tag/i.test(t.text));
-    if (tagNode && tagNode.children.length) {
-      const tag = extractTag(tagNode.children[0].text);
+    const tag = getTag(blockId);
+    if (tag) {
       const pages = getTitlesReferencingPagesInSameBlockTree([
         tag,
       ]).filter((t) => DAILY_NOTE_PAGE_REGEX.test(t));
@@ -53,10 +62,9 @@ const Timeline: React.FunctionComponent<TimelineProps> = ({ blockId }) => {
           return nodes.map(({ uid, text, children }) => ({
             date: p,
             uid,
-            text: text.replace(createTagRegex(tag), "").trim(),
-            body: children.reduce(
-              (prev, cur) => reduceChildren(prev, cur, 0),
-              ""
+            text: resolveRefs(text.replace(createTagRegex(tag), "")).trim(),
+            body: resolveRefs(
+              children.reduce((prev, cur) => reduceChildren(prev, cur, 0), "")
             ),
           }));
         })
@@ -71,31 +79,108 @@ const Timeline: React.FunctionComponent<TimelineProps> = ({ blockId }) => {
   const refresh = useCallback(() => {
     setTimelineElements(getTimelineElements());
   }, [setTimelineElements, getTimelineElements]);
+  const [showSettings, setShowSettings] = useState(false);
+  const toggleSettings = useCallback(() => setShowSettings(!showSettings), [
+    setShowSettings,
+    showSettings,
+  ]);
+  const [tagSetting, setTagSetting] = useState(() => getTag(blockId));
+  const onTagBlur = useCallback(() => {
+    const { blockUid } = getUidsFromId(blockId);
+    const tree = getTreeByBlockUid(blockUid);
+    const tagNode = tree.children.find((t) => /tag/i.test(t.text));
+    if (tagNode && tagNode.children.length) {
+      window.roamAlphaAPI.updateBlock({
+        block: { uid: tagNode.children[0].uid, string: tagSetting },
+      });
+    } else if (!tagNode) {
+      const uid = generateBlockUid();
+      window.roamAlphaAPI.createBlock({
+        location: { "parent-uid": blockUid, order: 0 },
+        block: { string: "tag", uid },
+      });
+      window.roamAlphaAPI.createBlock({
+        location: { "parent-uid": uid, order: 0 },
+        block: { string: tagSetting },
+      });
+    } else {
+      window.roamAlphaAPI.createBlock({
+        location: { "parent-uid": tagNode.uid, order: 0 },
+        block: { string: tagSetting },
+      });
+    }
+    setTimeout(() => setTimelineElements(getTimelineElements()), 1);
+  }, [blockId, setTimelineElements, getTimelineElements, tagSetting]);
   return (
-    <EditContainer
-      refresh={refresh}
-      blockId={blockId}
-      containerStyleProps={{ backgroundColor: "#CCCCCC", width: "150%" }}
-    >
-      <VerticalTimeline>
-        {timelineElements.map((t) => (
-          <VerticalTimelineElement
-            contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-            date={t.date}
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={
-              <Icon icon="calendar" style={{ height: "100%", width: "100%" }} />
-            }
-            key={t.uid}
-          >
-            <h3 className="vertical-timeline-element-title">{t.text}</h3>
-            <h4 className="vertical-timeline-element-subtitle">{t.uid}</h4>
-            <p>{t.body}</p>
-          </VerticalTimelineElement>
-        ))}
-      </VerticalTimeline>
-    </EditContainer>
+    <>
+      {showSettings && (
+        <div
+          style={{
+            backgroundColor: "white",
+            borderRadius: 3,
+            padding: 20,
+            border: "1px solid #333333",
+          }}
+        >
+          <h4>Settings:</h4>
+          <Label>
+            Tag
+            <InputGroup
+              value={tagSetting}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setTagSetting(e.target.value)
+              }
+              onBlur={onTagBlur}
+            />
+          </Label>
+        </div>
+      )}
+      <EditContainer
+        refresh={refresh}
+        blockId={blockId}
+        containerStyleProps={{ backgroundColor: "#CCCCCC", width: "175%" }}
+      >
+        <VerticalTimeline>
+          {timelineElements.map((t) => (
+            <VerticalTimelineElement
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date={t.date}
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={
+                <Icon
+                  icon="calendar"
+                  style={{ height: "100%", width: "100%" }}
+                />
+              }
+              key={t.uid}
+            >
+              <h3 className="vertical-timeline-element-title">
+                {t.text || "Empty Block"}
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">{t.uid}</h4>
+              <p>{t.body}</p>
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+        <Button
+          icon={"wrench"}
+          onClick={toggleSettings}
+          style={{
+            position: "absolute",
+            top: 8,
+            right: 88,
+            backgroundColor: showSettings
+              ? "rgba(115,134,148,0.3)"
+              : "transparent",
+            display: 'none',
+          }}
+          minimal
+        />
+      </EditContainer>
+    </>
   );
 };
 
