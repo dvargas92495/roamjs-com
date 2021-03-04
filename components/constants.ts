1055369a059d84c62d2a4e821388262bb11f5f79
David Vargas <dvargas92495@gmail.com>
2021-03-04 12:57:58 -0600

    Fix API URL

--- components/constants.ts
+++ components/constants.ts
@@ -1,10 +1,10 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const FLOSS_API_URL = process.env.NEXT_PUBLIC_FLOSS_API_URL;
-export const API_URL = process.env.NEXT_PUBLIC_REST_API_ID
-  ? `https://${process.env.NEXT_PUBLIC_REST_API_ID}.execute-api.us-east-1.amazonaws.com/production`
-  : "http://localhost:3003/dev";
-export const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY || "");
+export const API_URL = process.env.NEXT_PUBLIC_REST_API_URL;
+export const stripe = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY || ""
+);
 export type QueueItemResponse = {
   total: number;
   name: string;
