73942793416d68e1b29c7ce2691878ad3e5acdbd
David Vargas <dvargas92495@gmail.com>
2021-03-21 20:41:35 -0500

    Balance get

--- src/api/balance_get.ts
+++ src/api/balance_get.ts
@@ -1,22 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { getClerkUser, headers } from "../lambda-helpers";
+import { flossGet } from "../lambda-helpers";
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> =>
-  getClerkUser(event).then((user) =>
-    user
-      ? {
-          statusCode: 200,
-          body: JSON.stringify({
-            balance:
-              (user.publicMetadata as { balance?: string })?.balance || 0,
-          }),
-          headers: headers(event),
-        }
-      : {
-          statusCode: 401,
-          body: "No Active Session",
-          headers: headers(event),
-        }
-  );
+  flossGet({ event, path: "stripe-balance" });
