8b3f0a8e89ed396fba8e1485c54d6a460fa4083d
David Vargas <dvargas92495@gmail.com>
2021-04-06 10:41:57 -0600

    oauth generic

--- pages/oauth/index.tsx
+++ pages/oauth/index.tsx
@@ -1,15 +1,29 @@
 import { Root } from "@dvargas92495/ui";
-import React from "react";
-import TwitterAuth from "../../components/TwitterAuth";
+import React, { useEffect } from "react";
 
 const OauthPage = (): React.ReactElement => {
+  useEffect(() => {
+    if (window.opener && window.opener !== window) {
+      const query = new URLSearchParams(window.location.search);
+      const isAuth = query.get("auth");
+      if (isAuth) {
+        const params = {};
+        Array.from(query.entries())
+          .filter(([k]) => k !== "auth")
+          .forEach(([k, v]) => (params[k] = v));
+        window.opener.postMessage(
+          JSON.stringify(params),
+          "https://roamresearch.com"
+        );
+      }
+    }
+  }, []);
   return (
     <Root>
       <img
         src={"/images/logo-high-res.jpg"}
         style={{ maxHeight: "100vh", maxWidth: "100vw" }}
       />
-      <TwitterAuth />
     </Root>
   );
 };
