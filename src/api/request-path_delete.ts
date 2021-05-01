7cd8b7aa3499aee5c6d157e4b43a8658f4235d45
David Vargas <dvargas92495@gmail.com>
2021-04-30 22:20:10 -0600

    fix path delete endpoint

--- src/api/request-path_delete.ts
+++ src/api/request-path_delete.ts
@@ -1,57 +1,53 @@
 import { users } from "@clerk/clerk-sdk-node";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { getClerkUser, headers, s3, userError } from "../lambda-helpers";
+import {
+  authenticate,
+  headers,
+  s3,
+  serverError,
+  userError,
+} from "../lambda-helpers";
 
-export const handler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> =>
-  getClerkUser(event).then(async (user) => {
-    if (!user) {
-      return {
-        statusCode: 401,
-        body: "No Active Session",
-        headers: headers(event),
-      };
-    }
+export const handler = authenticate(async (event) => {
+  const { path } = event.queryStringParameters as { path?: string };
+  if (!path) {
+    return userError("Path is required", event);
+  }
+  const Objects = await s3
+    .listObjectsV2({ Bucket: "roamjs.com", Prefix: path })
+    .promise()
+    .then((r) => r.Contents.map((c) => ({ Key: c.Key })));
+  if (Objects.length === 0) {
+    return userError("Requested path is not being used", event);
+  }
 
-    const { path } = event.queryStringParameters as { path?: string };
-    if (!path) {
-      return userError("Path is required", event);
-    }
-    const Objects = await s3
-      .listObjectsV2({ Bucket: "roamjs.com", Prefix: path })
-      .promise()
-      .then((r) => r.Contents.map((c) => ({ Key: c.Key })));
-    if (Objects.length === 0) {
-      return userError("Requested path is not being used", event);
-    }
+  await s3
+    .deleteObjects({ Bucket: "roamjs.com", Delete: { Objects } })
+    .promise();
 
-    await s3
-      .deleteObjects({ Bucket: "roamjs.com", Delete: { Objects } })
-      .promise();
-
-    const id = user.id;
-    const publicMetadata = user.publicMetadata as {
-      developer: { paths?: string[] };
-    };
-    const paths = (publicMetadata.developer.paths || []).filter(
-      (p) => p !== path
-    );
-    return users
-      .updateUser(id, {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore https://github.com/clerkinc/clerk-sdk-node/pull/12#issuecomment-785306137
-        publicMetadata: JSON.stringify({
-          ...publicMetadata,
-          developer: {
-            ...publicMetadata.developer,
-            paths,
-          },
-        }),
-      })
-      .then(() => ({
-        statusCode: 200,
-        body: JSON.stringify({ paths }),
-        headers: headers(event),
-      }));
-  });
+  const id = event.headers.Authorization;
+  const user = await users.getUser(id);
+  const publicMetadata = user.publicMetadata as {
+    developer: { paths?: string[] };
+  };
+  const paths = (publicMetadata.developer.paths || []).filter(
+    (p) => p !== path
+  );
+  return users
+    .updateUser(id, {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore https://github.com/clerkinc/clerk-sdk-node/pull/12#issuecomment-785306137
+      publicMetadata: JSON.stringify({
+        ...publicMetadata,
+        developer: {
+          ...publicMetadata.developer,
+          paths,
+        },
+      }),
+    })
+    .then(() => ({
+      statusCode: 200,
+      body: JSON.stringify({ paths }),
+      headers: headers(event),
+    }))
+    .catch((e) => serverError(e.message, event));
+}, "developer");
