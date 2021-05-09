2e4497ab630b5afa3bae8f23312845afc69fc4ea
David Vargas <dvargas92495@gmail.com>
2021-05-08 22:30:42 -0600

    bump limits

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,7 +5,7 @@ module.exports = (env) => ({
     mode: 'production',
     performance: {
       hints: "error",
-      maxEntrypointSize: 4146296,
-      maxAssetSize: 4146296,
+      maxEntrypointSize: 5000000,
+      maxAssetSize: 5000000,
     },
 });
