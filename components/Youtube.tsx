539ae4e156f186095f4de562b3cc38242ac3451e
David Vargas <dvargas92495@gmail.com>
2021-05-11 13:46:48 -0600

    remove window.origin from youtube

--- components/Youtube.tsx
+++ components/Youtube.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import dynamic from "next/dynamic";
-
-const DynamicYoutube = dynamic(() => import("react-youtube"));
+import ReactYoutube from "react-youtube";
 
 const Youtube: React.FunctionComponent<{ id: string }> = ({ id }) => {
   return (
-    <DynamicYoutube
+    <ReactYoutube
       videoId={id}
       id={`roamjs-youtube-${id}`}
       opts={{
         width: "100%",
         playerVars: {
-          origin: window.location.origin,
+          origin: "https://roamjs.com",
           autoplay: 0 as const,
           rel: 0 as const,
         },
