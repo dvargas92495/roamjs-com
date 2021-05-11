b47c56988e56ef8b8916911b8c87cee3d88976da
David Vargas <dvargas92495@gmail.com>
2021-05-11 11:11:49 -0600

    Standing up a mock endpoint to setup the deployment

--- aws.tf
+++ aws.tf
@@ -339,6 +339,35 @@ resource "aws_api_gateway_rest_api" "lambda_api" {
   }
 }
 
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  path_part   = "mock"
+}
+
+resource "aws_api_gateway_method" "options" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "mock" {
+  rest_api_id          = aws_api_gateway_rest_api.lambda_api.id
+  resource_id          = aws_api_gateway_resource.resource.id
+  http_method          = aws_api_gateway_method.options.http_method
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
+
+  request_templates = {
+    "application/json" = jsonencode(
+        {
+            statusCode = 200
+        }
+    )
+  }
+}
+
 resource "aws_acm_certificate" "api" {
   domain_name       = "lambda.roamjs.com"
   validation_method = "DNS"
