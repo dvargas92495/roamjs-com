2a371df1582fd8fa7e29fa4d8d608cb88b272859
David Vargas <dvargas92495@gmail.com>
2021-05-11 09:51:35 -0600

    Standing up a lambdas api gateway for extensions

--- aws.tf
+++ aws.tf
@@ -322,6 +322,72 @@ resource "aws_dynamodb_table" "extensions" {
   }
 }
 
+resource "aws_api_gateway_rest_api" "lambda_api" {
+  name        = "roamjs-extensions"
+  
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
+  binary_media_types = [
+    "multipart/form-data",
+    "application/octet-stream"
+  ]
+
+  tags = {
+    Application = "Roam JS Extensions"
+  }
+}
+
+resource "aws_acm_certificate" "api" {
+  domain_name       = "lambda.roamjs.com"
+  validation_method = "DNS"
+
+  tags = {
+    Application = "Roam JS Extensions"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "api_cert" {
+  name    = tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_type
+  zone_id = data.aws_route53_zone.zone.id
+  records = [tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_value]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "api" {
+  certificate_arn         = aws_acm_certificate.api.arn
+  validation_record_fqdns = [aws_route53_record.api_cert.fqdn]
+}
+
+resource "aws_api_gateway_domain_name" "api" {
+  certificate_arn = aws_acm_certificate_validation.api.certificate_arn
+  domain_name     = "lambda.roamjs.com"
+}
+
+resource "aws_route53_record" "api" {
+  name    = aws_api_gateway_domain_name.api.domain_name
+  type    = "A"
+  zone_id = module.aws_static_site.route53_zone_id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_api_gateway_domain_name.api.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.api.cloudfront_zone_id
+  }
+}
+
+resource "aws_api_gateway_base_path_mapping" "api" {
+  api_id      = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = aws_api_gateway_deployment.production.stage_name
+  domain_name = aws_api_gateway_domain_name.api.domain_name
+}
+
 provider "github" {
     owner = "dvargas92495"
 }
