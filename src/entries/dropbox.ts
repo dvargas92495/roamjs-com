561750f327fdfdf88f1f4b940f18a0586c11d600
David Vargas <dvargas92495@gmail.com>
2021-05-13 17:13:42 -0500

    Upload slash command

--- src/entries/dropbox.ts
+++ src/entries/dropbox.ts
@@ -6,6 +6,7 @@ import {
   createBlock,
   createHTMLObserver,
   getTreeByPageName,
+  getUids,
   updateBlock,
 } from "roam-client";
 import { Dropbox } from "dropbox";
@@ -52,78 +53,156 @@ runExtension(ID, () => {
     },
   });
 
+  const uploadToDropbox = ({
+    files,
+    getLoadingUid,
+    e,
+  }: {
+    files: FileList;
+    getLoadingUid: () => string;
+    e: Event;
+  }) => {
+    const fileToUpload = files[0];
+    if (fileToUpload) {
+      const oauth = getOauth(getTreeByPageName(CONFIG));
+      if (oauth !== "{}") {
+        const { access_token } = JSON.parse(oauth);
+        const dbx = new Dropbox({ accessToken: access_token });
+        const uid = getLoadingUid();
+        const reader = new FileReader();
+
+        reader.readAsBinaryString(fileToUpload);
+
+        reader.onloadend = () =>
+          dbx
+            .filesUpload({
+              path: `/${fileToUpload.name}`,
+              contents: fileToUpload,
+              autorename: true,
+            })
+            .then((r) =>
+              dbx
+                .sharingListSharedLinks({ path: r.result.path_display })
+                .then((l) =>
+                  l.result.links.length
+                    ? l.result.links[0].url
+                    : dbx
+                        .sharingCreateSharedLinkWithSettings({
+                          path: r.result.path_display,
+                          settings: {
+                            requested_visibility: { ".tag": "public" },
+                          },
+                        })
+                        .then((c) => c.result.url)
+                )
+            )
+            .then((r) => {
+              const url = r.replace(/dl=0$/, "raw=1");
+              updateBlock({
+                uid,
+                text: `![](${url})`,
+              });
+            })
+            .catch((e) => {
+              updateBlock({
+                uid,
+                text: "Failed to upload file to dropbox. Email support@roamjs.com with the error below:",
+              });
+              createBlock({
+                parentUid: uid,
+                node: { text: JSON.stringify(e) },
+              });
+            })
+            .finally(() => {
+              Array.from(document.getElementsByClassName("dnd-drop-bar"))
+                .map((c) => c as HTMLDivElement)
+                .forEach((c) => (c.style.display = "none"));
+            });
+        e.stopPropagation();
+        e.preventDefault();
+      }
+    }
+  };
+
   createHTMLObserver({
     tag: "DIV",
     className: "dnd-drop-area",
     callback: (d: HTMLDivElement) => {
       d.addEventListener("drop", (e) => {
-        const fileToUpload = e.dataTransfer.files[0];
-        if (fileToUpload) {
-          const oauth = getOauth(getTreeByPageName(CONFIG));
-          if (oauth !== "{}") {
-            const { access_token } = JSON.parse(oauth);
-            const dbx = new Dropbox({ accessToken: access_token });
+        uploadToDropbox({
+          files: e.dataTransfer.files,
+          getLoadingUid: () => {
             const { parentUid, offset } = getDropUidOffset(d);
-            const uid = createBlock({
+            return createBlock({
               parentUid,
               order: offset,
               node: { text: "Loading..." },
             });
-            const reader = new FileReader();
-
-            reader.readAsBinaryString(fileToUpload);
-
-            reader.onloadend = () =>
-              dbx
-                .filesUpload({
-                  path: `/${fileToUpload.name}`,
-                  contents: fileToUpload,
-                  autorename: true,
-                })
-                .then((r) =>
-                  dbx
-                    .sharingListSharedLinks({ path: r.result.path_display })
-                    .then((l) =>
-                      l.result.links.length
-                        ? l.result.links[0].url
-                        : dbx
-                            .sharingCreateSharedLinkWithSettings({
-                              path: r.result.path_display,
-                              settings: {
-                                requested_visibility: { ".tag": "public" },
-                              },
-                            })
-                            .then((c) => c.result.url)
-                    )
-                )
-                .then((r) => {
-                  const url = r.replace(/dl=0$/, "raw=1");
-                  updateBlock({
-                    uid,
-                    text: `![](${url})`,
-                  });
-                })
-                .catch((e) => {
-                  updateBlock({
-                    uid,
-                    text:
-                      "Failed to upload file to dropbox. Email support@roamjs.com with the error below:",
-                  });
-                  createBlock({
-                    parentUid: uid,
-                    node: { text: JSON.stringify(e) },
-                  });
-                })
-                .finally(() => {
-                  Array.from(document.getElementsByClassName("dnd-drop-bar"))
-                    .map((c) => c as HTMLDivElement)
-                    .forEach((c) => (c.style.display = "none"));
-                });
-            e.stopPropagation();
-            e.preventDefault();
-          }
-        }
+          },
+          e,
+        });
       });
     },
+  });
+
+  const textareaRef: { current: HTMLTextAreaElement } = {
+    current: null,
+  };
+
+  createHTMLObserver({
+    tag: "TEXTAREA",
+    className: "rm-block-input",
+    callback: (t: HTMLTextAreaElement) => (textareaRef.current = t),
+  });
+
+  /*new MutationObserver((mrs) => {
+    mrs
+      .flatMap((mr) => Array.from(mr.addedNodes))
+      .filter((n) => n.nodeName === "INPUT")
+      .map((n) => n as HTMLInputElement)
+      .filter((n) => n.type === "file")
+      .forEach((n) => {
+        n.replaceWith(n.cloneNode(false));
+        n.onchange = (e) => {
+          uploadToDropbox({
+            files: (e.target as HTMLInputElement).files,
+            getLoadingUid: () => {
+              const { blockUid } = getUids(textareaRef.current);
+              return updateBlock({
+                text: "Loading...",
+                uid: blockUid,
+              });
+            },
+            e,
+          });
+        };
+      });
+  }).observe(document.body, { childList: true });*/
+
+  document.addEventListener("click", (e) => {
+    const target = e.target as HTMLInputElement;
+    if (
+      target.tagName === "INPUT" &&
+      target.parentElement === document.body &&
+      target.type === "file"
+    ) {
+      target.addEventListener(
+        "change",
+        (e) => {
+          uploadToDropbox({
+            files: (e.target as HTMLInputElement).files,
+            getLoadingUid: () => {
+              const { blockUid } = getUids(textareaRef.current);
+              return updateBlock({
+                text: "Loading...",
+                uid: blockUid,
+              });
+            },
+            e,
+          });
+        },
+        { capture: true }
+      );
+    }
   });
 });
