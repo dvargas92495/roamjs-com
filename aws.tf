faee9df5889762a88ca2974c524ee1879b0dbd33
David Vargas <dvargas92495@gmail.com>
2021-05-25 16:49:33 -0600

    static site 2.2.7

--- aws.tf
+++ aws.tf
@@ -117,7 +117,7 @@ provider "aws" {
 
 module "aws_static_site" {
   source  = "dvargas92495/static-site/aws"
-  version = "2.2.6"
+  version = "2.2.7"
 
   domain = "roamjs.com"
   redirects = ["roam.davidvargas.me"]
