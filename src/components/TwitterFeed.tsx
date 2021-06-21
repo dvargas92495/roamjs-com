5b6f268021e4517782638d2c2d1f536927bca5c9
David Vargas <dvargas92495@gmail.com>
2021-06-21 14:45:57 -0700

    Switch accounts on Twitter feed (Closes #722)

--- src/components/TwitterFeed.tsx
+++ src/components/TwitterFeed.tsx
@@ -1,4 +1,11 @@
-import { Button, Checkbox, Dialog, Intent, Spinner } from "@blueprintjs/core";
+import {
+  Button,
+  Checkbox,
+  Classes,
+  Dialog,
+  Intent,
+  Spinner,
+} from "@blueprintjs/core";
 import axios from "axios";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import ReactDOM from "react-dom";
@@ -9,7 +16,7 @@ import {
   toRoamDate,
   toRoamDateUid,
 } from "roam-client";
-import { getOauth } from "roamjs-components";
+import { getOauth, getOauthAccounts, MenuItemSelect } from "roamjs-components";
 import subDays from "date-fns/subDays";
 import startOfDay from "date-fns/startOfDay";
 import endOfDay from "date-fns/endOfDay";
@@ -64,6 +71,8 @@ const TwitterFeed = ({
   const [tweets, setTweets] = useState<Tweet[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const accounts = useMemo(() => getOauthAccounts("twitter"), []);
+  const [activeAccount, setActiveAccount] = useState(accounts[0]);
   const onClose = useCallback(() => {
     ReactDOM.unmountComponentAtNode(
       document.getElementById("roamjs-twitter-feed")
@@ -86,7 +95,10 @@ const TwitterFeed = ({
     onClose();
   }, [onClose, date, title]);
   useEffect(() => {
-    const oauth = getOauth("twitter");
+    setLoading(true);
+    setTweets([]);
+    setError('');
+    const oauth = getOauth("twitter", activeAccount);
     if (oauth === "{}") {
       setError(
         "Need to log in with Twitter to use Daily Twitter Feed! Head to roam/js/twitter page to log in."
@@ -110,7 +122,7 @@ const TwitterFeed = ({
       })
       .catch((r) => setError(r.response?.data || r.message))
       .finally(() => setLoading(false));
-  }, [setTweets, yesterday]);
+  }, [setTweets, yesterday, activeAccount]);
   const onClick = useCallback(() => {
     createBlock({
       parentUid: toRoamDateUid(date),
@@ -144,46 +156,63 @@ const TwitterFeed = ({
         ) : error ? (
           <span style={{ color: "darkred" }}>{error}</span>
         ) : (
-          <>
-            <div
-              style={{
-                maxHeight: 760,
-                overflowY: "scroll",
-                paddingBottom: 16,
-                paddingLeft: 4,
-              }}
-            >
-              {tweets.map((tweet) => (
-                <Checkbox
-                  key={tweet.id}
-                  checked={tweet.checked}
-                  onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                    setTweets(
-                      tweets.map((t) =>
-                        t.id === tweet.id
-                          ? {
-                              ...t,
-                              checked: (e.target as HTMLInputElement).checked,
-                            }
-                          : t
-                      )
+          <div
+            style={{
+              maxHeight: 760,
+              overflowY: "scroll",
+              paddingBottom: 16,
+              paddingLeft: 4,
+            }}
+          >
+            {tweets.map((tweet) => (
+              <Checkbox
+                key={tweet.id}
+                checked={tweet.checked}
+                onChange={(e: React.FormEvent<HTMLInputElement>) =>
+                  setTweets(
+                    tweets.map((t) =>
+                      t.id === tweet.id
+                        ? {
+                            ...t,
+                            checked: (e.target as HTMLInputElement).checked,
+                          }
+                        : t
                     )
-                  }
-                >
-                  <TweetLabel id={tweet.id} />
-                </Checkbox>
-              ))}
-              {!tweets.length && <span>No tweets liked.</span>}
-            </div>
-            <Button
-              onClick={onClick}
-              intent={Intent.PRIMARY}
-              style={{ marginTop: 16 }}
-            >
-              {tweets.length ? "IMPORT" : "OK"}
-            </Button>
-          </>
+                  )
+                }
+              >
+                <TweetLabel id={tweet.id} />
+              </Checkbox>
+            ))}
+            {!tweets.length && <span>No tweets liked.</span>}
+          </div>
         )}
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <div
+          className={Classes.DIALOG_FOOTER_ACTIONS}
+          style={{
+            justifyContent: "space-between",
+            alignItems: "baseline",
+          }}
+        >
+          {accounts.length > 1 && (
+            <MenuItemSelect
+              activeItem={activeAccount}
+              items={accounts}
+              onItemSelect={(a) => setActiveAccount(a)}
+              disabled={loading}
+            />
+          )}
+          <Button
+            onClick={onClick}
+            intent={Intent.PRIMARY}
+            style={{ marginTop: 16 }}
+            disabled={loading || !!error}
+          >
+            {tweets.length ? "IMPORT" : "OK"}
+          </Button>
+        </div>
       </div>
     </Dialog>
   );
