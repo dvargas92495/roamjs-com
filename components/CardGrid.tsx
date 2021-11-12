36d8b7e40561386df6e06659340a81e50fa5af7b
David Vargas <dvargas92495@gmail.com>
2021-11-12 16:53:16 -0500

    fix extensions page styling

--- components/CardGrid.tsx
+++ components/CardGrid.tsx
@@ -1,5 +1,4 @@
 import Grid, { GridSize } from "@material-ui/core/Grid";
-import styled from "@material-ui/core/styles/styled";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
@@ -8,35 +7,6 @@ import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 import Fade from "@material-ui/core/Fade";
 
-const PreviewMedia = styled(CardMedia)({
-  borderRadius: 4,
-});
-
-const PreviewContainer = styled("div")({
-  padding: 8,
-  position: "relative",
-  flexGrow: 1,
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-});
-
-const PreviewOverlay = styled("div")(({ theme }) => ({
-  backgroundColor: "rgba(255,255,255,0.95)",
-  color: theme.palette.text.secondary,
-  justifyContent: "center",
-  left: 0,
-  padding: 10,
-  textAlign: "center",
-  top: 0,
-  position: "absolute",
-  width: "100%",
-  height: "100%",
-  alignItems: "center",
-  display: "flex",
-  fontSize: 12,
-}));
-
 const Preview: React.FunctionComponent<{
   image: string;
   title: string;
@@ -44,12 +14,14 @@ const Preview: React.FunctionComponent<{
 }> = ({ image, title, description }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [showDescription, setShowDescription] = useState(false);
-  const onMouseEnter = useCallback(() => setShowDescription(true), [
-    setShowDescription,
-  ]);
-  const onMouseLeave = useCallback(() => setShowDescription(false), [
-    setShowDescription,
-  ]);
+  const onMouseEnter = useCallback(
+    () => setShowDescription(true),
+    [setShowDescription]
+  );
+  const onMouseLeave = useCallback(
+    () => setShowDescription(false),
+    [setShowDescription]
+  );
   const [height, setHeight] = useState(140);
   useEffect(() => {
     const dummyImage = new Image();
@@ -71,34 +43,48 @@ const Preview: React.FunctionComponent<{
     };
   }, [containerRef, image]);
   return (
-    <PreviewContainer
+    <div
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
       ref={containerRef}
+      style={{
+        padding: 8,
+        position: "relative",
+        flexGrow: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
     >
-      <PreviewMedia image={image} title={title} style={{ height }} />
+      <CardMedia
+        image={image}
+        title={title}
+        style={{ height, borderRadius: 4 }}
+      />
       <Fade in={showDescription} timeout={750}>
-        <PreviewOverlay>{description}</PreviewOverlay>
+        <div
+          style={{
+            backgroundColor: "rgba(255,255,255,0.95)",
+            color: "#888888",
+            justifyContent: "center",
+            left: 0,
+            padding: 10,
+            textAlign: "center",
+            top: 0,
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            alignItems: "center",
+            display: "flex",
+            fontSize: 12,
+          }}
+        >
+          {description}
+        </div>
       </Fade>
-    </PreviewContainer>
+    </div>
   );
 };
-
-const StyledCard = styled(Card)({
-  maxWidth: 345,
-  height: "100%",
-});
-
-const StyledLink = styled(Link)({
-  display: "flex",
-  flexDirection: "column",
-  height: "100%",
-});
-
-const StyledTitle = styled(Typography)({
-  margin: 0,
-  textAlign: "center",
-});
 
 const CardGrid: React.FunctionComponent<{
   items: { image: string; title: string; description: string; href: string }[];
@@ -108,14 +94,31 @@ const CardGrid: React.FunctionComponent<{
     <Grid container spacing={2}>
       {items.map(({ image, title, description, href }) => (
         <Grid item xs={width} key={title}>
-          <StyledCard>
-            <StyledLink href={href}>
+          <Card
+            style={{
+              maxWidth: 345,
+              height: "100%",
+            }}
+          >
+            <Link
+              href={href}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                height: "100%",
+              }}
+            >
               <Preview title={title} description={description} image={image} />
               <CardContent style={{ padding: 8 }}>
-                <StyledTitle variant="h6" style={{fontSize: 16}}>{title}</StyledTitle>
+                <Typography
+                  variant="h6"
+                  style={{ fontSize: 16, margin: 0, textAlign: "center" }}
+                >
+                  {title}
+                </Typography>
               </CardContent>
-            </StyledLink>
-          </StyledCard>
+            </Link>
+          </Card>
         </Grid>
       ))}
     </Grid>
