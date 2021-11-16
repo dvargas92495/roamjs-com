f94851e723bca976f06df9ce8dbf38257f561ec9
David Vargas <dvargas92495@gmail.com>
2021-11-16 12:11:37 -0500

    stand up a dev extensions dynamo table

--- aws.tf
+++ aws.tf
@@ -274,7 +274,8 @@ resource "aws_route53_record" "gitbook" {
 }
 
 resource "aws_dynamodb_table" "extensions" {
-  name           = "RoamJSExtensions"
+  for_each       = ["RoamJSExtensions", "RoamJSExtensionsDev"]
+  name           = each.value
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
 
