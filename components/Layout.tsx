5afc80c32feb4bbdd4933fa4b7b6f21a1fdfbf27
David Vargas <dvargas92495@gmail.com>
2021-11-19 11:26:45 -0500

    img typo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,7 +13,7 @@ export type LayoutProps = {
 export const defaultLayoutProps = {
   title: "RoamJS",
   description: "Become a Roam Power User",
-  img: "https://roamjs.com/images/logo-low-res.jpg",
+  img: "https://roamjs.com/images/logo-low-res.png",
 };
 
 const UserIconDynamic = dynamic(() => import("../components/UserIcon"), {
