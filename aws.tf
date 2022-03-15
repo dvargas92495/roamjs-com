0ddc56792ed4c6adfb49eb2dcc663354e6e3c747
David Vargas <dvargas92495@gmail.com>
2022-03-14 19:11:42 -0600

    add user index to dynamo extensions tables

--- aws.tf
+++ aws.tf
@@ -283,9 +283,23 @@ resource "aws_dynamodb_table" "extensions" {
     type = "S"
   }
 
+  attribute {
+    name = "user"
+    type = "S"
+  }
+
   global_secondary_index {
     hash_key           = "state"
     name               = "state-index"
+    non_key_attributes = []
+    projection_type    = "ALL"
+    read_capacity      = 0
+    write_capacity     = 0
+  }
+
+  global_secondary_index {
+    hash_key           = "user"
+    name               = "user-index"
     non_key_attributes = []
     projection_type    = "ALL"
     read_capacity      = 0
@@ -312,9 +326,23 @@ resource "aws_dynamodb_table" "extensions-dev" {
     type = "S"
   }
 
+  attribute {
+    name = "user"
+    type = "S"
+  }
+
   global_secondary_index {
     hash_key           = "state"
     name               = "state-index"
+    non_key_attributes = []
+    projection_type    = "ALL"
+    read_capacity      = 0
+    write_capacity     = 0
+  }
+
+  global_secondary_index {
+    hash_key           = "user"
+    name               = "user-index"
     non_key_attributes = []
     projection_type    = "ALL"
     read_capacity      = 0
