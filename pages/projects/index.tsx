581b5943e04254366b9e4c8519b0c56939c4e8d1
David Vargas <dvargas92495@gmail.com>
2022-03-21 23:59:04 -0600

    Start workinpublic integration

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,63 +1,19 @@
-import { Queue, StringField } from "@dvargas92495/ui";
-import Link from "next/link";
-import React, { useCallback, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import StandardLayout from "../../components/StandardLayout";
-import axios from "axios";
-import { FLOSS_API_URL } from "../../components/constants";
 import { defaultLayoutProps } from "../../components/Layout";
-import ProjectFundButton from "../../components/ProjectFundButton";
 
-type ProjectResponse = {
-  progress: number;
-  target: number;
-  name: string;
-  uuid: string;
-};
-
-const ProjectsPage = (): JSX.Element => {
-  const [search, setSearch] = useState("");
-  const loadItems = useCallback(
-    () =>
-      axios
-        .get(
-          `${FLOSS_API_URL}/projects?tenant=${process.env.NEXT_PUBLIC_FLOSS_TENANT}`
-        )
-        .then((r) => r.data.projects || []),
-    []
-  );
-  const mapper = useCallback(
-    (item: ProjectResponse) => ({
-      avatar: (
-        <div style={{ minWidth: 100 }}>
-          {Math.floor((item.progress / item.target) * 100)}% Funded
-        </div>
-      ),
-      primary: item.name,
-      secondary: (
-        <span>
-          <Link href={`projects/${item.uuid}`}>Details</Link>
-        </span>
-      ),
-      action: (
-        <ProjectFundButton
-          uuid={item.uuid}
-          name={item.name}
-          onSuccess={() =>
-            Promise.resolve(
-              window.location.assign(`/projects/${item.uuid}?checkout=true`)
-            )
-          }
-        />
-      ),
-      key: item.uuid,
-    }),
-    []
-  );
-  const filter = useCallback(
-    (item) =>
-      !search || item.primary.toLowerCase().indexOf(search.toLowerCase()) > -1,
-    [search]
-  );
+const ProjectsPage = (): ReactElement => {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    if (!mounted) {
+      setMounted(true);
+    } else if (!Array.from(document.getElementsByTagName('script')).some(s => s.src.includes('workinpublic'))) {
+      const script = document.createElement('script');
+      script.src = "https://workinpublic.io/board/84cc166b-9f98-4640-a2cb-6db8b28bfe0d.js"
+      script.async = true;
+      document.head.appendChild(script);
+    }
+  }, [mounted, setMounted]);
   return (
     <StandardLayout
       title={"Projects | RoamJS"}
@@ -66,23 +22,8 @@ const ProjectsPage = (): JSX.Element => {
       }
       img={defaultLayoutProps.img}
     >
-      <div style={{ marginBottom: 16, padding: "0 16px" }}>
-        <StringField
-          value={search}
-          setValue={setSearch}
-          label={"Search"}
-          fullWidth
-        />
-      </div>
-      <div style={{ padding: 8, width: "100%", height: 512 }}>
-        <Queue
-          title={"Projects"}
-          loadItems={loadItems}
-          mapper={mapper}
-          filter={filter}
-          subheader={"Fund a project to help it reach its funding goal!"}
-        />
-      </div>
+      <h2 style={{textAlign: 'center'}}>This Page Is Under Construction. Come Back Soon!</h2>
+      <div id="84cc166b-9f98-4640-a2cb-6db8b28bfe0d" key="84cc166b-9f98-4640-a2cb-6db8b28bfe0d"></div>
     </StandardLayout>
   );
 };
