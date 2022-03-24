e7cc31668de7d4db90dbbb9732a6f89eeb9487b2
David Vargas <dvargas92495@gmail.com>
2022-03-24 13:46:43 -0600

    fix publish using dynamo paths

--- src/api/publish_post.ts
+++ src/api/publish_post.ts
@@ -1,57 +1,69 @@
-import { users } from "@clerk/clerk-sdk-node";
-import { APIGatewayProxyHandler } from "aws-lambda";
+import type { APIGatewayProxyHandler } from "aws-lambda";
 import AWS from "aws-sdk";
-import { authenticate, headers, userError } from "../lambda-helpers";
+import {
+  authenticateDeveloper,
+  headers,
+  userError,
+  dynamo,
+  TableName,
+} from "../lambda-helpers";
 
 const sts = new AWS.STS({ apiVersion: "2011-06-15" });
 
-export const handler: APIGatewayProxyHandler = authenticate((event) => {
-  const userId = event.headers.Authorization;
-  const { path } = JSON.parse(event.body || "{}");
-  return users
-    .getUser(userId)
-    .then(
-      (u) =>
-        (u.publicMetadata as { developer: { paths: string[] } })["developer"]
-          .paths || []
-    )
-    .then((paths) =>
-      paths.includes(path.replace(/^([^/]+)\/(.*)$/, '$1'))
-        ? sts
-            .assumeRole({
-              RoleArn: process.env.LAMBDA_ROLE,
-              DurationSeconds: 900,
-              RoleSessionName: `publish-${userId}`,
-              Policy: JSON.stringify({
-                Version: "2012-10-17",
-                Statement: [
-                  {
-                    Sid: "PutPaths",
-                    Effect: "Allow",
-                    Action: "s3:PutObject",
-                    Resource: [`arn:aws:s3:::roamjs.com/${path}/*`],
-                  },
-                  {
-                    Sid: "InvalidateCache",
-                    Effect: "Allow",
-                    Action: [
-                      "cloudfront:CreateInvalidation",
-                      "cloudfront:GetInvalidation",
-                    ],
-                    Resource: process.env.CLOUDFRONT_ARN,
-                  },
-                ],
-              }),
-            })
-            .promise()
-            .then((creds) => ({
-              statusCode: 200,
-              body: JSON.stringify({
-                credentials: creds.Credentials,
-                distributionId: /\/(.*)$/.exec(process.env.CLOUDFRONT_ARN)?.[1],
-              }),
-              headers: headers(event),
-            }))
-        : userError(`User not authorized to publish to path ${path}`, event)
-    );
-}, "developer");
+export const handler: APIGatewayProxyHandler = authenticateDeveloper(
+  (event) => {
+    const userId = event.headers.Authorization;
+    const { path } = JSON.parse(event.body || "{}");
+    return dynamo
+      .query({
+        TableName,
+        IndexName: "user-index",
+        KeyConditionExpression: "#u = :u",
+        ExpressionAttributeNames: { "#u": "user" },
+        ExpressionAttributeValues: { ":u": { S: userId } },
+      })
+      .promise()
+      .then((r) => r.Items.map((i) => i.id?.S))
+      .then((paths) =>
+        paths.includes(path.replace(/^([^/]+)\/(.*)$/, "$1"))
+          ? sts
+              .assumeRole({
+                RoleArn: process.env.LAMBDA_ROLE,
+                DurationSeconds: 900,
+                RoleSessionName: `publish-${userId}`,
+                Policy: JSON.stringify({
+                  Version: "2012-10-17",
+                  Statement: [
+                    {
+                      Sid: "PutPaths",
+                      Effect: "Allow",
+                      Action: "s3:PutObject",
+                      Resource: [`arn:aws:s3:::roamjs.com/${path}/*`],
+                    },
+                    {
+                      Sid: "InvalidateCache",
+                      Effect: "Allow",
+                      Action: [
+                        "cloudfront:CreateInvalidation",
+                        "cloudfront:GetInvalidation",
+                      ],
+                      Resource: process.env.CLOUDFRONT_ARN,
+                    },
+                  ],
+                }),
+              })
+              .promise()
+              .then((creds) => ({
+                statusCode: 200,
+                body: JSON.stringify({
+                  credentials: creds.Credentials,
+                  distributionId: /\/(.*)$/.exec(
+                    process.env.CLOUDFRONT_ARN
+                  )?.[1],
+                }),
+                headers: headers(event),
+              }))
+          : userError(`User not authorized to publish to path ${path}`, event)
+      );
+  }
+);
--- src/lambda-helpers.ts
+++ src/lambda-helpers.ts
@@ -212,64 +212,6 @@ export const generateToken = (): { encrypted: string; value: string } => {
   };
 };
 
-const findUser = async (predicate: (u: User) => boolean): Promise<User> => {
-  let offset = 0;
-  while (offset < 10000) {
-    const us = await users.getUserList({ limit: 100, offset });
-    const user = us.find(predicate);
-    if (user) {
-      return user;
-    }
-    if (us.length < 100) {
-      return;
-    }
-    offset += us.length;
-  }
-};
-
-export const getUserFromEvent = (
-  Authorization: string,
-  service: string,
-  dev?: boolean
-): Promise<User> => {
-  if (dev) {
-    setClerkApiKey(process.env.CLERK_DEV_API_KEY);
-  } else {
-    setClerkApiKey(process.env.CLERK_API_KEY);
-  }
-  const [userId, token] =
-    Authorization.length === 32 || Authorization.includes(":")
-      ? // the old ways of generating tokens did not have user id encoded, so we query all users
-        [
-          null,
-          Authorization.split(":").slice(-1)[0],
-          Authorization.split(":").slice(-1)[0],
-        ]
-      : [
-          Buffer.from(Authorization, "base64").toString().split(":")[0],
-          Authorization,
-          Buffer.from(Authorization, "base64").toString().split(":")[1],
-        ];
-
-  return userId
-    ? users
-        .getUser(`user_${userId}`)
-        .then((user) =>
-          (user.publicMetadata as { [s: string]: { token: string } })?.[service]
-            ?.token === token
-            ? user
-            : undefined
-        )
-        .catch(() => {
-          return undefined;
-        })
-    : findUser(
-        (user) =>
-          (user.publicMetadata as { [s: string]: { token: string } })?.[service]
-            ?.token === token
-      );
-};
-
 export const authenticateUser = (
   Authorization: string,
   dev?: boolean
@@ -300,51 +242,27 @@ export const authenticateUser = (
     .catch(() => undefined);
 };
 
-export const authenticate =
-  (
-    handler: APIGatewayProxyHandler,
-    inputService?: "staticSite" | "social" | "developer"
-  ): APIGatewayProxyHandler =>
+export const authenticateDeveloper =
+  (handler: APIGatewayProxyHandler): APIGatewayProxyHandler =>
   (event, ctx, callback) => {
-    const service = inputService || event.queryStringParameters.service;
     const Authorization =
       event.headers.Authorization || event.headers.authorization || "";
     const dev = !!event.headers["x-roamjs-dev"];
 
-    return Promise.all([
-      authenticateUser(Authorization, dev),
-      getUserFromEvent(Authorization, service, dev),
-    ]).then(([userv2, legacyUser]) => {
-      const user = userv2 || legacyUser;
+    return authenticateUser(Authorization, dev).then((user) => {
       if (!user) {
-        console.log(
-          "Failed to authenticate",
-          Authorization.slice(-5),
-          service,
-          dev
-        );
         return {
           statusCode: 401,
-          body: "Invalid token",
+          body: "Invalid Developer token",
           headers: headers(event),
         };
       }
-      const publicMetadata = user.publicMetadata;
-      const serviceData = (
-        publicMetadata as {
-          [s: string]: { authenticated: boolean };
-        }
-      )[service];
-      if (!serviceData.authenticated) {
-        users.updateUser(user.id, {
-          publicMetadata: {
-            ...publicMetadata,
-            [service]: {
-              ...serviceData,
-              authenticated: true,
-            },
-          },
-        });
+      if (!user.publicMetadata["developer"]) {
+        return {
+          statusCode: 403,
+          body: "User has not signed up for the RoamJS Developer extension",
+          headers: headers(event),
+        };
       }
       event.headers.Authorization = user.id;
       const result = handler(event, ctx, callback);
