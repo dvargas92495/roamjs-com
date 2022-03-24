0ea37e95325379f8b3dd92621b436b5b13788953
David Vargas <dvargas92495@gmail.com>
2022-03-23 23:35:01 -0600

    Add Freelancing section

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -7,9 +7,14 @@ const ProjectsPage = (): ReactElement => {
   useEffect(() => {
     if (!mounted) {
       setMounted(true);
-    } else if (!Array.from(document.getElementsByTagName('script')).some(s => s.src.includes('workinpublic'))) {
-      const script = document.createElement('script');
-      script.src = "https://workinpublic.io/board/84cc166b-9f98-4640-a2cb-6db8b28bfe0d.js"
+    } else if (
+      !Array.from(document.getElementsByTagName("script")).some((s) =>
+        s.src.includes("workinpublic")
+      )
+    ) {
+      const script = document.createElement("script");
+      script.src =
+        "https://workinpublic.io/board/84cc166b-9f98-4640-a2cb-6db8b28bfe0d.js";
       script.async = true;
       document.head.appendChild(script);
     }
@@ -22,8 +27,26 @@ const ProjectsPage = (): ReactElement => {
       }
       img={defaultLayoutProps.img}
     >
-      <h2 style={{textAlign: 'center'}}>This Page Is Under Construction. Come Back Soon!</h2>
-      <div id="84cc166b-9f98-4640-a2cb-6db8b28bfe0d" key="84cc166b-9f98-4640-a2cb-6db8b28bfe0d"></div>
+      <h2 style={{ textAlign: "center" }}>
+        This Page Is Under Construction. Come Back Soon!
+      </h2>
+      <div
+        id="84cc166b-9f98-4640-a2cb-6db8b28bfe0d"
+        key="84cc166b-9f98-4640-a2cb-6db8b28bfe0d"
+      ></div>
+      <h2 id="freelancing">Freelancing</h2>
+      <p>
+        Have a personal feature request? Or would like company's product to
+        integrate with Roam? Reach out in the widget on the bottom right or
+        [book a call](https://calendly.com/dvargas92495/roamjs) to inquire about
+        working together on the project
+      </p>
+      <p>
+        We operate on a milestone basis at $2,000/milestone. As our client, we
+        would work together to agree upon milestones ahead of time which we size
+        at about 4 hours of work, but they often take longer. Bug fixes that
+        come up during and after the project are free.
+      </p>
     </StandardLayout>
   );
 };
