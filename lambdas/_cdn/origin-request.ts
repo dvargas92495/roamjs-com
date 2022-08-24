3f5eee4afd6f1818ffb4ffa252cb391915deb815
David Vargas <dvargas92495@gmail.com>
2022-08-24 15:33:16 -0400

    Map multiplayer to samepage

--- lambdas/_cdn/origin-request.ts
+++ lambdas/_cdn/origin-request.ts
@@ -10,15 +10,24 @@ const movedUris = [
   "twitter",
 ].map((e) => `/${e}.js`);
 
+// In the future, move this to the roamjs static site generator
+const REDIRECTS: Record<string, string> = {
+  "/multiplayer/main.js": "/samepage/main.js",
+  // "/roam42/main.js": "/roam42/main.js",
+};
+
 export const handler: CloudFrontRequestHandler = (event, _, callback) => {
   const request = event.Records[0].cf.request;
   const olduri = request.uri;
   if (movedUris.includes(olduri)) {
     request.uri = olduri.replace(/\.js$/, "/main.js");
-    console.log("Mapped", olduri, "to", request.uri);
   } else if (olduri.endsWith("/") && olduri.length > 1) {
     request.uri = olduri.replace(/\/$/, "");
-    console.log("Mapped", olduri, "to", request.uri);
+  } else if (REDIRECTS[olduri]) {
+    request.uri = REDIRECTS[olduri];
+  } else {
+    return callback(null, request);
   }
+  console.log("Mapped", olduri, "to", request.uri);
   return callback(null, request);
 };
