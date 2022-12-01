e75870dd8546222e58745fa16b31aba1c6768847
David Vargas <dvargas92495@gmail.com>
2022-12-01 12:49:57 -0500

    migrate maps to roam depot

--- src/entries/maps.ts
+++ src/entries/maps.ts
@@ -2,10 +2,14 @@ import createButtonObserver from "roamjs-components/dom/createButtonObserver";
 import runExtension from "roamjs-components/util/runExtension";
 import { render } from "../components/Maps";
 
-runExtension("maps", () => {
-  createButtonObserver({
-    shortcut: "maps",
-    attribute: "leaflet",
-    render,
-  });
+runExtension({
+  extensionId: "maps",
+  migratedTo: "MapBox",
+  run: () => {
+    createButtonObserver({
+      shortcut: "maps",
+      attribute: "leaflet",
+      render,
+    });
+  },
 });
